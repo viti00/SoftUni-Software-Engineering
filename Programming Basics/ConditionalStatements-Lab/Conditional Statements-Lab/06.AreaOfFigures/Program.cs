8124c1f7b90e4506da0223cb5dc62a37f84f782d
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-12 12:26:07 +0200

    Add files via upload

---
+++ Programming Basics/ConditionalStatements-Lab/Conditional Statements-Lab/06.AreaOfFigures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape = Console.ReadLine();
+            double area = 0;
+            if (shape == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+            }
+            else if (shape == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+            else if (shape == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                area = Math.PI * (r * r);
+            }
+            else if (shape == "triangle")
+            {
+                double x = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                area = x * h / 2;
+            }
+            Console.WriteLine(Math.Round(area, 3));
+        }
+    }
+}
---
+++ Programming Basics/ConditionalStatements-Lab/Conditional Statements-Lab/07.ToyShop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07.ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double travelPrice = double.Parse(Console.ReadLine());
+            int puzzleNum = int.Parse(Console.ReadLine());
+            int speakToysNum = int.Parse(Console.ReadLine());
+            int bearsNum = int.Parse(Console.ReadLine());
+            int minionsNum = int.Parse(Console.ReadLine());
+            int trucksNum = int.Parse(Console.ReadLine());
+            double toysPrice = puzzleNum * 2.60 + speakToysNum * 3 + bearsNum * 4.10 + minionsNum * 8.20 + trucksNum * 2;
+            double toysNum = puzzleNum + speakToysNum + bearsNum + minionsNum + trucksNum;
+            double priceAfterDiscount = 0;
+            if (toysNum >= 50)
+            {
+                priceAfterDiscount = toysPrice - (toysPrice * 0.25);
+            }
+            else
+            {
+                priceAfterDiscount = toysPrice;
+            }
+            double rent = priceAfterDiscount * 0.10;
+            double profit = priceAfterDiscount - rent;
+            if (profit - travelPrice >= 0)
+            {
+                double more = profit - travelPrice;
+                Console.WriteLine($"Yes! {more:F2} lv left.");
+            }
+            else if (travelPrice - profit > 0)
+            {
+                double needed = travelPrice - profit;
+                Console.WriteLine($"Not enough money! {needed:F2} lv needed.");
+            }
+        }
+    }
+}
