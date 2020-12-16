db4754e053a5e89ffa725a3d9a98b056ec21ba01
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-16 13:39:41 +0200

    Add files via upload

---
+++ Programming Basics/For Loop Exercise/ForLoopExercise/02.HalfSumElement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            int sumWhitoutMaxNumber = sum - max;
+            if (max == sumWhitoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + max);
+            }
+            else
+            {
+                int diff = Math.Abs(max - sumWhitoutMaxNumber);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            }
+        }
+    }
+}
