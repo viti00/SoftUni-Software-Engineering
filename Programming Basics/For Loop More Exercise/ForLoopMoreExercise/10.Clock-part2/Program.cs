477c4fe4afb50e51f0a25e648e8cdfa2052fe530
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-16 13:58:51 +0200

    Add files via upload

---
+++ Programming Basics/For Loop More Exercise/ForLoopMoreExercise/10.Clock-part2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10.Clock_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour <= 23; hour++)
+            {
+                for (int min = 0; min <= 59; min++)
+                {
+                    for (int sec = 0; sec <= 59; sec++)
+                    {
+                        string result = "";
+                        result += $"{hour} : {min} : {sec}";
+                        Console.WriteLine(result);
+                    }
+                }
+            }
+        }
+    }
+}
