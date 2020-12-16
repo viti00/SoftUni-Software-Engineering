64b38462e2a4bc2ab7c83e5ac3b88ddabbfc0b57
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-16 13:35:10 +0200

    Add files via upload

---
+++ Programming Basics/For Loop - Lab pt.2/ForLoop-LabPt.2/08.NumberSequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxValue = int.MinValue;
+            int minValue = int.MaxValue;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num < minValue)
+                {
+                    minValue = num;
+                }
+                if (num > maxValue)
+                {
+                    maxValue = num;
+                }
+            }
+            Console.WriteLine($"Max number: {maxValue}");
+            Console.WriteLine($"Min number: {minValue}");
+        }
+    }
+}
---
+++ Programming Basics/For Loop - Lab pt.2/ForLoop-LabPt.2/11.CleverLily/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _11.CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double wmPrice = double.Parse(Console.ReadLine());
+            double toysPrice = double.Parse(Console.ReadLine());
+            double savedMoney = 0.0;
+            double money = 0.0;
+            double sum = 0.0;
+            double moneyTotal = 0.0;
+            int toys = 0;
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += 10;
+                    money += sum - 1;
+                    savedMoney = money;
+                    moneyTotal = savedMoney;
+                }
+                else
+                {
+                    toys++;
+                }
+            }
+            moneyTotal += toys * toysPrice;
+            if (moneyTotal >= wmPrice)
+            {
+                double more = moneyTotal - wmPrice;
+                Console.WriteLine($"Yes! {more:F2}");
+            }
+            else if (moneyTotal < wmPrice)
+            {
+                double needed = wmPrice - moneyTotal;
+                Console.WriteLine($"No! {needed:F2}");
+            }
+        }
+    }
+}
