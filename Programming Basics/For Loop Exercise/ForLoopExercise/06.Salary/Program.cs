083dafe8895c0af03a9386c2f8f1d10f75ecdbb6
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-16 13:46:34 +0200

    Add files via upload

---
+++ Programming Basics/For Loop Exercise/ForLoopExercise/06.Salary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _06.Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counttabs = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+
+            //double restsalary = 0;
+
+            for (int n = 0; n < counttabs; n++)
+            {
+                string website = Console.ReadLine();
+
+
+                switch (website)
+                {
+                    case "Facebook":
+
+                        salary = salary - 150;
+                        break;
+                    case "Instagram":
+
+                        salary = salary - 100;
+                        break;
+
+                    case "Reddit":
+
+                        salary = salary - 50;
+                        break;
+                }
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
