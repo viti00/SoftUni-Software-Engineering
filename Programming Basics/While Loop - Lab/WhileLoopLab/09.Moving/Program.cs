6d1f2540eae32f204daa11d893f6dfc410cfc6ba
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-17 13:13:07 +0200

    Add files via upload

---
+++ Programming Basics/While Loop - Lab/WhileLoopLab/09.Moving/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09.Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shirochina = int.Parse(Console.ReadLine());
+            int duljina = int.Parse(Console.ReadLine());
+            int visochina = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            double sum = 0.0;
+            bool isSpaceEnough = true;
+            double kubMetriStaq = shirochina * duljina * visochina;
+            while (input != "Done")
+            {
+                sum = int.Parse(input);
+                kubMetriStaq -= sum;
+                if (kubMetriStaq <= 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(kubMetriStaq)} Cubic meters more.");
+                    isSpaceEnough = false;
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (isSpaceEnough)
+            {
+                Console.WriteLine($"{kubMetriStaq} Cubic meters left.");
+            }
+        }
+    }
+}
