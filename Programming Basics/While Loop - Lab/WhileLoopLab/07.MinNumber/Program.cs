62b5a953c717b1cf532a4eda12e63484e6ea97c1
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-17 13:03:42 +0200

    Add files via upload

---
+++ Programming Basics/While Loop - Lab/WhileLoopLab/07.MinNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int min = int.MaxValue;
+            while (input != "Stop")
+            {
+                int nums = int.Parse(input);
+                if (nums < min)
+                {
+                    min = nums;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
