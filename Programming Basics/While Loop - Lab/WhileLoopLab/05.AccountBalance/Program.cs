f3a4d1cb123cb0c247b423878c59adf05e05e764
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-17 13:01:38 +0200

    Add files via upload

---
+++ Programming Basics/While Loop - Lab/WhileLoopLab/05.AccountBalance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05.AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double increase = 0.0;
+            double bankAcount = 0.0;
+            while (input != "NoMoreMoney")
+            {
+                increase = double.Parse(input);
+                if (increase < 0)
+                {
+                    Console.WriteLine($"Invalid operation!");
+                    break;
+                }
+                bankAcount += increase;
+                Console.WriteLine($"Increase: {increase:f2}");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {bankAcount:F2}");
+        }
+    }
+}
