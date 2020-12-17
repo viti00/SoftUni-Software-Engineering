f86a71ac52cb71a62995a1d63f91ff729a6dde9d
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-17 13:04:46 +0200

    Add files via upload

---
+++ Programming Basics/While Loop - Lab/WhileLoopLab/08.Graduationpt.2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08.Graduationpt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grades = 1;
+            double sum = 0.0;
+            int excludet = 0;
+            while (grades <= 12)
+            {
+                double rating = double.Parse(Console.ReadLine());
+                if (rating >= 4.00)
+                {
+                    grades++;
+                    sum += rating;
+                }
+                else if (rating < 4.00)
+                {
+                    excludet++;
+                    if (excludet > 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            double averageSum = sum / 12;
+            if (grades == 13)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {averageSum:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {grades} grade");
+            }
+        }
+    }
+}
