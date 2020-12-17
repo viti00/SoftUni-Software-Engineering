de4f0687bc9a8695c2a75841de8e426792751b97
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-17 13:02:29 +0200

    Add files via upload

---
+++ Programming Basics/While Loop - Lab/WhileLoopLab/06.MaxNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int max = int.MinValue;
+            while (input != "Stop")
+            {
+                int nums = int.Parse(input);
+                if (nums > max)
+                {
+                    max = nums;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
