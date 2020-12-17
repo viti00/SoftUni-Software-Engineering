9de9557b9bd0407add59d240eb9d870c9211e7d6
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-17 12:59:03 +0200

    Add files via upload

---
+++ Programming Basics/While Loop - Lab/WhileLoopLab/03.Sum Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int k = 1;
+            int numSum = 0;
+            while (k <= number)
+            {
+                int nums = int.Parse(Console.ReadLine());
+                numSum += nums;
+                if (numSum >= number)
+                {
+                    Console.WriteLine(numSum);
+                    break;
+                }
+            }
+        }
+    }
+}
