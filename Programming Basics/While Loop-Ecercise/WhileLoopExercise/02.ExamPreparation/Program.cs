352d060d570156bd4bafce318f0cb27dd1574ed9
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-18 15:29:19 +0200

    Add files via upload

---
+++ Programming Basics/While Loop-Ecercise/WhileLoopExercise/02.ExamPreparation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02.ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int failedCountGrade = int.Parse(Console.ReadLine());
+            int failedTimes = 0;
+            int solvedProblemCount = 0;
+            double sum = 0;
+            string lastProblem = "";
+            bool isFailed = true;
+            while (failedTimes < failedCountGrade)
+            {
+                string problemName = Console.ReadLine();
+                if ("Enough" == problemName)
+                {
+                    isFailed = false;
+                    break;
+                }
+                int grade = int.Parse(Console.ReadLine());
+                if (grade <= 4)
+                {
+                    failedTimes++;
+                }
+                sum += grade;
+                solvedProblemCount++;
+                lastProblem = problemName;
+            }
+            if (isFailed)
+            {
+                Console.WriteLine($"You need a break, {failedCountGrade} poor grades.");
+            }
+            else
+            {
+                Console.WriteLine($"Average score: {sum / solvedProblemCount:f2}");
+                Console.WriteLine($"Number of problems: {solvedProblemCount}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+            }
+        }
+    }
+}
