ac68fbb298c6a32ee4d8212e8bbeaf4ad09eb5e2
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-18 15:22:45 +0200

    Add files via upload

---
+++ Programming Basics/While Loop-Ecercise/WhileLoopExercise/01.OldBooks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01.OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int counter = 0;
+            bool isBookFound = false;
+            string nextBook = Console.ReadLine();
+            while (nextBook != "No More Books")
+            {
+                if (nextBook == book)
+                {
+                    isBookFound = true;
+                    break;
+                }
+                counter++;
+                nextBook = Console.ReadLine();
+            }
+            if (isBookFound)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+            }
+        }
+    }
+}
