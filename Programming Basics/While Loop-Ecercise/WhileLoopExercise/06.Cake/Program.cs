04cf5b81536c7755c300696d57452bd41c925ab7
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-18 15:35:51 +0200

    Add files via upload

---
+++ Programming Basics/While Loop-Ecercise/WhileLoopExercise/06.Cake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+            int total = width * hight;
+            int pieceCounter = 0;
+            while (total >= pieceCounter)
+            {
+                string pieces = Console.ReadLine();
+                if (pieces == "STOP")
+                {
+                    Console.WriteLine($"{total - pieceCounter} pieces are left.");
+                    break;
+                }
+                pieceCounter += int.Parse(pieces);
+            }
+            if (pieceCounter > total)
+            {
+                Console.WriteLine($"No more cake left! You need {pieceCounter - total} pieces more.");
+            }
+        }
+    }
+}
