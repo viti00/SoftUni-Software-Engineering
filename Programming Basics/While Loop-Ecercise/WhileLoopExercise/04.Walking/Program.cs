07bf368814683f1d8d81f5bcd3cfe25361e584cf
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-18 15:32:51 +0200

    Add files via upload

---
+++ Programming Basics/While Loop-Ecercise/WhileLoopExercise/04.Walking/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _04.Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int sum = 0;
+
+
+
+            while (command != "Going home")
+            {
+                sum += int.Parse(command);
+
+
+
+                if (sum < 10000)
+                {
+
+
+                }
+                if (sum >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{sum - 10000} steps over the goal!");
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+
+            if (command == "Going home")
+            {
+                command = Console.ReadLine();
+                sum += int.Parse(command);
+
+                if (sum < 10000)
+                {
+                    Console.WriteLine($"{10000 - sum} more steps to reach goal.");
+
+                }
+                else if (sum >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{sum - 10000} steps over the goal!");
+                }
+
+            }
+        }
+    }
+}
