dfdcd737dbe9924f04703d98144c44004ff5d66d
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-19 17:28:08 +0200

    Add files via upload

---
+++ Programming Basics/While Loop-MoreExercise/WhileLoopMoreExercise/05.AverageNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05.AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int dupf = n;
+            double sum = 0;
+
+            while (dupf != 0)
+            {
+                dupf = dupf - 1;
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine($"{ sum / n:F2}");
+        }
+    }
+}
