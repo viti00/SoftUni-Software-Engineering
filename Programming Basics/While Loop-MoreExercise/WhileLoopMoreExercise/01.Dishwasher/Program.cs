8bd2755154ceaf2d0e15622da6629d2059c12fa7
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-19 17:14:46 +0200

    Add files via upload

---
+++ Programming Basics/While Loop-MoreExercise/WhileLoopMoreExercise/01.Dishwasher/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01.Dishwasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottlesDetergent = int.Parse(Console.ReadLine());
+            bottlesDetergent *= 750;
+            string numberOfContainers = string.Empty;
+            int containers = 0;
+            int counter = 0;
+            int numberOfDishesWashed = 0;
+            int numberOfPotsWashed = 0;
+
+            while (true)
+            {
+                numberOfContainers = Console.ReadLine();
+                if (numberOfContainers == "End")
+                {
+                    break;
+                }
+                counter++;
+                if (counter % 3 == 0)
+                {
+                    containers = int.Parse(numberOfContainers);
+                    numberOfPotsWashed += containers;
+                    bottlesDetergent -= containers * 15;
+                }
+                else
+                {
+                    containers = int.Parse(numberOfContainers);
+                    numberOfDishesWashed += containers;
+                    bottlesDetergent -= containers * 5;
+                }
+                if (bottlesDetergent < 0)
+                {
+                    Console.WriteLine($"Not enough detergent, {Math.Abs(bottlesDetergent)} ml. more necessary!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Detergent was enough!");
+            Console.WriteLine($"{numberOfDishesWashed} dishes and {numberOfPotsWashed} pots were washed.");
+            Console.WriteLine($"Leftover detergent {bottlesDetergent} ml.");
+        }
+    }
+}
