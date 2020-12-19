a0a76381e1367ddbd57c60e1c2e4bb0b73847297
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-19 17:26:29 +0200

    Add files via upload

---
+++ Programming Basics/While Loop-MoreExercise/WhileLoopMoreExercise/04.NumbersDividedBy3WithoutReminder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.NumbersDividedBy3WithoutReminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = 1;
+            while (startNum <= 100)
+            {
+                if (startNum % 3 == 0)
+                {
+                    Console.WriteLine(startNum);
+                }
+                startNum++;
+            }
+        }
+    }
+}
