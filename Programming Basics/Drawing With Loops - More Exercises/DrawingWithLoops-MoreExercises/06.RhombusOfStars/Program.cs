daf0788b193d79cd30ea73ad2f44a2d2134fcfdb
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-21 13:41:22 +0200

    Add files via upload

---
+++ Programming Basics/Drawing With Loops - More Exercises/DrawingWithLoops-MoreExercises/06.RhombusOfStars/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _06.RhombusOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (i == 1)
+                {
+                    Console.Write("*");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int k = 1; k <= i; k++)
+                    {
+                        Console.Write("* ");
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = n - i; j >= 1; j--)
+                {
+                    Console.Write(" ");
+                }
+                if (i == 1)
+                {
+                    Console.Write("*");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int k = 1; k <= i; k++)
+                    {
+                        Console.Write("* ");
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
