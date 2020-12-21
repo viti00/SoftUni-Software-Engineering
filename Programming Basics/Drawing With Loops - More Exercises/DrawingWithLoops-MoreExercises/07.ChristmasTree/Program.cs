549b11439886f523531ae3d566cebd1a46c7f32c
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-21 13:41:53 +0200

    Add files via upload

---
+++ Programming Basics/Drawing With Loops - More Exercises/DrawingWithLoops-MoreExercises/07.ChristmasTree/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _07.ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 0; row <= n; row++)
+            {
+                for (int col = 1; col <= n - row; col++)
+                {
+                    Console.Write(" ");
+                }
+                if (row == 0)
+                {
+                    Console.Write(" |");
+                }
+                else
+                {
+                    for (int i = 0; i < row; i++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write(" |");
+                }
+                Console.Write(" ");
+                if (row == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int k = 0; k < row; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
