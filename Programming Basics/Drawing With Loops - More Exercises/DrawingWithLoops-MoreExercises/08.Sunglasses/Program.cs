fb629cd533c7b04053735715d568c62676b66e8c
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-21 13:42:52 +0200

    Add files via upload

---
+++ Programming Basics/Drawing With Loops - More Exercises/DrawingWithLoops-MoreExercises/08.Sunglasses/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _08.Sunglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= n; row++)
+            {
+                Console.Write("*");
+                for (int col = 1; col <= n * 2 - 2; col++)
+                {
+                    if (row == 1 || row == n)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("/");
+                    }
+                }
+                Console.Write("*");
+                if (n % 2 == 0)
+                {
+                    if (row == n / 2)
+                    {
+                        for (int i = 1; i <= n * 5 - n * 4; i++)
+                        {
+                            Console.Write("|");
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 1; i <= n * 5 - n * 4; i++)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                else if (n % 2 == 1)
+                {
+                    if (row == n / 2 + 1)
+                    {
+                        for (int i = 1; i <= n * 5 - n * 4; i++)
+                        {
+                            Console.Write("|");
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 1; i <= n * 5 - n * 4; i++)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.Write("*");
+                for (int col = 1; col <= n * 2 - 2; col++)
+                {
+                    if (row == 1 || row == n)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("/");
+                    }
+                }
+                Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
