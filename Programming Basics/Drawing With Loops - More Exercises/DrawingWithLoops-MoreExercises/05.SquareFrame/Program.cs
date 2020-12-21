515b3bcdb7fea9cb5bf0f382001e623142819f76
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-21 13:40:39 +0200

    Add files via upload

---
+++ Programming Basics/Drawing With Loops - More Exercises/DrawingWithLoops-MoreExercises/05.SquareFrame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05.SquareFrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == 1 || i == n)
+                {
+                    Console.Write("+" + " ");
+                }
+                else
+                {
+                    Console.Write("|" + " ");
+                }
+                for (int j = 1; j <= n; j++)
+                {
+                    if (j > 1 && j < n)
+                    {
+                        Console.Write("-" + " ");
+                    }
+                    else if (j == n && i == 1 || j == n && i == n)
+                    {
+                        Console.Write("+");
+                    }
+                    else if (j == n && i > 1)
+                    {
+                        Console.Write("|");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
