847efc5e06a3766f5608795a84772b24808a0304
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-22 16:02:56 +0200

    Add files via upload

---
+++ Programming Basics/Nested Loops - Lab/NestedLoops-Lab/02.MultiplicationTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 10; n++)
+            {
+                for (int m = 1; m <= 10; m++)
+                {
+                    int final = n * m;
+                    string result = "";
+                    result = $"{n} * {m} = {final}";
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
