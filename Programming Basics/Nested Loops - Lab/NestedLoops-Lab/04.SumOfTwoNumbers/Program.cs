c9010ba8688801ece1c785df7cf4c538916a2179
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-22 16:06:56 +0200

    Add files via upload

---
+++ Programming Basics/Nested Loops - Lab/NestedLoops-Lab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04.SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int exit = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int combinationCounter = 0;
+            bool combinationFound = false;
+            for (int i = start; i <= exit; i++)
+            {
+                for (int j = start; j <= exit; j++)
+                {
+                    combinationCounter++;
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combinationCounter} ({i} + {j} = {magicNumber})");
+                        combinationFound = true;
+                        break;
+                    }
+                }
+                if (combinationFound)
+                {
+                    break;
+                }
+            }
+            if (!combinationFound)
+            {
+                Console.WriteLine($"{combinationCounter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
