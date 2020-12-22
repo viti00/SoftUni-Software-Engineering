781b627eeef79ec70e57c6f73f1e487bbdbe704a
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-22 16:07:37 +0200

    Add files via upload

---
+++ Programming Basics/Nested Loops - Lab/NestedLoops-Lab/05.Travelling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double cost = double.Parse(Console.ReadLine());
+                double earnedSavings = 0;
+                while (cost > earnedSavings)
+                {
+                    earnedSavings += double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
