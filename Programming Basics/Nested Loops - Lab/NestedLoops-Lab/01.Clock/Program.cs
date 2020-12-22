42b6521a9592c4c43cf5283460a2702931a55634
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-22 15:56:02 +0200

    Add files via upload

---
+++ Programming Basics/Nested Loops - Lab/NestedLoops-Lab/01.Clock/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hours = 0; hours <= 23; hours++)
+            {
+                for (int min = 0; min <= 59; min++)
+                {
+                    string result = "";
+                    result = $"{hours}:{min}";
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
