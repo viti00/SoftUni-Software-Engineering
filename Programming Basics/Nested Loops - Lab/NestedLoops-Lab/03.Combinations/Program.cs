eb1a1911109eaaea30e5afa10d6530cfed48aa7e
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-22 16:05:13 +0200

    Add files via upload

---
+++ Programming Basics/Nested Loops - Lab/NestedLoops-Lab/03.Combinations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03.Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int validOperation = 0;
+            for (int x1 = 0; x1 <= input; x1++)
+            {
+                for (int x2 = 0; x2 <= input; x2++)
+                {
+                    for (int x3 = 0; x3 <= input; x3++)
+                    {
+                        int product = x1 + x2 + x3;
+                        if (product == input)
+                        {
+                            validOperation++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(validOperation);
+        }
+    }
+}
