d6234f2ee301681cec3c4eea333cd09a87fcc508
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-23 12:27:11 +0200

    Add files via upload

---
+++ Programming Basics/Nested Loops - Exercise/NestedLoops-Exercise/02.EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02.EqualSumsEvenOddPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            for (int i = num1; i <= num2; i++)
+            {
+                string currentNum = i.ToString();
+                int even = 0;
+                int odd = 0;
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentNum[j].ToString());
+                    if (j % 2 == 0)
+                    {
+                        even += currentDigit;
+                    }
+                    else
+                    {
+                        odd += currentDigit;
+                    }
+                }
+                if (odd == even)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
