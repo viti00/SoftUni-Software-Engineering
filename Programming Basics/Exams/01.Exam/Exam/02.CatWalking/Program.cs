1390e39673280d8fe99a84a4edb89884eaae1db6
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-24 13:09:56 +0200

    Add files via upload

---
+++ Programming Basics/Exams/01.Exam/Exam/02.CatWalking/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02.CatWalking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+            int calories = int.Parse(Console.ReadLine());
+            double totalTime = minutes * times;
+            double burnCalories = totalTime * 5;
+            double averageCalories = calories * 0.5;
+            if (burnCalories >= averageCalories)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {burnCalories}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {burnCalories}.");
+            }
+        }
+    }
+}
