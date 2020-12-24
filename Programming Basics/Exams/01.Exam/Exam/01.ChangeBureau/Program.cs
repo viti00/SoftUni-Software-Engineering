4823cdbddc482a62e98bc9950e79f8d068a3470a
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-24 13:03:40 +0200

    Add files via upload

---
+++ Programming Basics/Exams/01.Exam/Exam/01.ChangeBureau/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.ChangeBureau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bitcoins = double.Parse(Console.ReadLine());
+            double yuans = double.Parse(Console.ReadLine());
+            double comission = double.Parse(Console.ReadLine());
+            double bitcoinToLeva = bitcoins * 1168;
+            double yuanToDolars = yuans * 0.15;
+            double dolarToLeva = yuanToDolars * 1.76;
+            double totalSumInLeva = bitcoinToLeva + dolarToLeva;
+            double totalSumInEuro = totalSumInLeva / 1.95;
+            double comissions = totalSumInEuro * (comission / 100);
+            double finalPrice = totalSumInEuro - comissions;
+            Console.WriteLine($"{finalPrice:f2}");
+        }
+    }
+}
