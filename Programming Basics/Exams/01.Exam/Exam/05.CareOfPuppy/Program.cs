cd8d2ae21b57434417fb23d7037066748e1c1540
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-24 13:13:13 +0200

    Add files via upload

---
+++ Programming Basics/Exams/01.Exam/Exam/05.CareOfPuppy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05.CareOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodInKg = int.Parse(Console.ReadLine());
+            int foodInGr = foodInKg * 1000;
+            string input = "";
+            double gramCounter = 0.0;
+            while (input != "Adopted")
+            {
+                input = Console.ReadLine();
+                if (input == "Adopted")
+                {
+                    break;
+                }
+                gramCounter += int.Parse(input);
+            }
+            if (gramCounter <= foodInGr)
+            {
+                double more = foodInGr - gramCounter;
+                Console.WriteLine($"Food is enough! Leftovers: {more} grams.");
+            }
+            else
+            {
+                double needed = gramCounter - foodInGr;
+                Console.WriteLine($"Food is not enough. You need {needed} grams more.");
+            }
+        }
+    }
+}
