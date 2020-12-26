473a2ab4c4bb597dc0941abf445b0b2291b1f126
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-26 12:18:51 +0200

    Add files via upload

---
+++ Programming Basics/Exams/02.Exam/Exam/02.MountainRun/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.MountainRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSec = double.Parse(Console.ReadLine());
+            double distanceInM = double.Parse(Console.ReadLine());
+            double timesForMInSec = double.Parse(Console.ReadLine());
+            double time = distanceInM * timesForMInSec;
+            double delay = Math.Floor(distanceInM / 50) * 30;
+            double totalTime = time + delay;
+            if (totalTime < recordInSec)
+            {
+                Console.WriteLine($"Yes! The new record is {totalTime:F2} seconds.");
+            }
+            else
+            {
+                double fail = totalTime - recordInSec;
+                Console.WriteLine($"No! He was {fail:F2} seconds slower.");
+            }
+        }
+    }
+}
