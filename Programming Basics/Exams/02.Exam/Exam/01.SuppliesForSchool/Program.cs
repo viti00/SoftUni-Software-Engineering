23e1ebd84d2feb2631683465d0682722b0631e1f
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-26 12:13:06 +0200

    Add files via upload

---
+++ Programming Basics/Exams/02.Exam/Exam/01.SuppliesForSchool/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.SuppliesForSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pen = int.Parse(Console.ReadLine());
+            int markers = int.Parse(Console.ReadLine());
+            double litres = double.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+            double priceForPen = pen * 5.80;
+            double priceForMarkers = markers * 7.20;
+            double preparation = litres * 1.20;
+            double totalPrice = priceForPen + priceForMarkers + preparation;
+            double finalPrice = totalPrice - ((totalPrice * discount) / 100);
+            Console.WriteLine($"{finalPrice:f3}");
+        }
+    }
+}
