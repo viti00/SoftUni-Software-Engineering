e515e93008d62d617e3b291bd8606c55a342c4fe
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-28 15:11:16 +0200

    Add files via upload

---
+++ Programming Basics/Exams/03.Exam/Exam/05.DivisionWithoutRemainder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05.DivisionWithoutRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1Counter = 0;
+            double p2Counter = 0;
+            double p3Counter = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    p1Counter++;
+                }
+                if (num % 3 == 0)
+                {
+                    p2Counter++;
+                }
+                if (num % 4 == 0)
+                {
+                    p3Counter++;
+                }
+            }
+            double p1Percent = (p1Counter / n) * 100;
+            double p2Percent = (p2Counter / n) * 100;
+            double p3Percent = (p3Counter / n) * 100;
+
+            Console.WriteLine($"{p1Percent:F2}%");
+            Console.WriteLine($"{p2Percent:F2}%");
+            Console.WriteLine($"{p3Percent:F2}%");
+        }
+    }
+}
