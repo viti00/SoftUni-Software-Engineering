b50abce333b5e9dccfa65cc091100478280789bb
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-28 15:02:23 +0200

    Add files via upload

---
+++ Programming Basics/Exams/03.Exam/Exam/01.FoodDelivery/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenu = int.Parse(Console.ReadLine());
+            int fishMenu = int.Parse(Console.ReadLine());
+            int vegetarianMenu = int.Parse(Console.ReadLine());
+
+            double chickenMenuPrice = 10.35;
+            double fishMenuPrice = 12.40;
+            double vegetarianMenuPrice = 8.15;
+
+            double totalPrice = chickenMenu * chickenMenuPrice + fishMenu * fishMenuPrice + vegetarianMenu * vegetarianMenuPrice;
+            double dessertPrice = totalPrice * 0.2;
+            double finalPrice = totalPrice + dessertPrice + 2.50;
+            Console.WriteLine($"Total: {finalPrice:f2}");
+        }
+    }
+}
