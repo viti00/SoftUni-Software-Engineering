8390f4162d2fd7fcd3430167020a76a5c8478f4b
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-29 12:40:07 +0200

    Add files via upload

---
+++ Programming Basics/Exams/04.Exam/Exam/04.HoneyCombs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04.HoneyCombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bees = int.Parse(Console.ReadLine());
+            int flowers = int.Parse(Console.ReadLine());
+            double honeyCombs = bees * flowers * 0.21;
+            double fullHoneyCombs = honeyCombs / 100;
+            double ostatuk = honeyCombs % 100;
+            Console.WriteLine($"{Math.Floor(fullHoneyCombs)} honeycombs filled.");
+            Console.WriteLine($"{ostatuk:f2} grams of honey left.");
+        }
+    }
+}
