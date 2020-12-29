270dbf3b1a1c19ba6332d07f9c7b44a34391b38c
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-29 12:37:50 +0200

    Add files via upload

---
+++ Programming Basics/Exams/04.Exam/Exam/02.BeehiveRole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02.BeehiveRole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iq = int.Parse(Console.ReadLine());
+            int strong = int.Parse(Console.ReadLine());
+            string sex = Console.ReadLine();
+            if (iq >= 80 && strong >= 80 && sex == "female")
+            {
+                Console.WriteLine("Queen Bee");
+
+            }
+            else if (iq >= 80)
+            {
+                Console.WriteLine("Repairing Bee");
+            }
+            else if (iq >= 60 && iq < 80)
+            {
+                Console.WriteLine("Cleaning Bee");
+            }
+            else if (strong >= 80 && sex == "male")
+            {
+                Console.WriteLine("Drone Bee");
+            }
+            else if (strong >= 60)
+            {
+                Console.WriteLine("Guard Bee");
+            }
+            else
+            {
+                Console.WriteLine("Worker Bee");
+            }
+        }
+    }
+}
