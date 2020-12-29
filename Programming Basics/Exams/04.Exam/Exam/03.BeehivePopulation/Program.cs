d0b5d6f23bf8dd5338c6e5bbc62486729540a933
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-29 12:39:03 +0200

    Add files via upload

---
+++ Programming Basics/Exams/04.Exam/Exam/03.BeehivePopulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03.BeehivePopulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int population = int.Parse(Console.ReadLine());
+            int years = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= years; i++)
+            {
+                population += population / 10 * 2;
+                if (i % 5 == 0)
+                {
+                    population -= population / 50 * 5;
+                }
+                population -= population / 20 * 2;
+            }
+            Console.WriteLine($"Beehive population: {population}");
+        }
+    }
+}
