4a4634c907c615c2b151312dd480733c3c9c7252
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-29 12:32:01 +0200

    Add files via upload

---
+++ Programming Basics/Exams/04.Exam/Exam/01.BeehiveDeffense/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01.BeehiveDeffense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bees = int.Parse(Console.ReadLine());
+            int hp = int.Parse(Console.ReadLine());
+            int attack = int.Parse(Console.ReadLine());
+            while (hp > 0 || bees > 0)
+            {
+                bees -= attack;
+                hp -= bees * 5;
+                if (bees < 100)
+                {
+                    if (bees <= 0)
+                    {
+                        bees = 0;
+                        break;
+                    }
+                    break;
+                }
+                if (hp <= 0)
+                {
+                    hp = 0;
+                    break;
+                }
+            }
+            if (hp > 0)
+            {
+                Console.WriteLine($"The bear stole the honey! Bees left {bees}.");
+            }
+            else if (bees > 0)
+            {
+                Console.WriteLine($"Beehive won! Bees left {bees}.");
+            }
+        }
+    }
+}
