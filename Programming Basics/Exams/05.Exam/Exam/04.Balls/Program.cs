a6596ee78e09bcbbd30a227c79c82f714e5ccc44
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-30 16:22:23 +0200

    Add files via upload

---
+++ Programming Basics/Exams/05.Exam/Exam/04.Balls/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _04.Balls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int pointCounter = 0;
+            int redBallsCounter = 0;
+            int orangeBallsCounter = 0;
+            int yellowBallsCounter = 0;
+            int whiteBallsCounter = 0;
+            int blackBallsCounter = 0;
+            int otherBallsCounter = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                string color = Console.ReadLine();
+                if (color == "red")
+                {
+                    pointCounter += 5;
+                    redBallsCounter++;
+                }
+                else if (color == "orange")
+                {
+                    pointCounter += 10;
+                    orangeBallsCounter++;
+                }
+                else if (color == "yellow")
+                {
+                    pointCounter += 15;
+                    yellowBallsCounter++;
+                }
+                else if (color == "white")
+                {
+                    pointCounter += 20;
+                    whiteBallsCounter++;
+                }
+                else if (color == "black")
+                {
+                    pointCounter /= 2;
+                    blackBallsCounter++;
+                }
+                else
+                {
+                    pointCounter += 0;
+                    otherBallsCounter++;
+                }
+            }
+            Console.WriteLine($"Total points: {pointCounter}");
+            Console.WriteLine($"Points from red balls: {redBallsCounter}");
+            Console.WriteLine($"Points from orange balls: {orangeBallsCounter}");
+            Console.WriteLine($"Points from yellow balls: {yellowBallsCounter}");
+            Console.WriteLine($"Points from white balls: {whiteBallsCounter}");
+            Console.WriteLine($"Other colors picked: {otherBallsCounter}");
+            Console.WriteLine($"Divides from black balls: {blackBallsCounter}");
+        }
+    }
+}
