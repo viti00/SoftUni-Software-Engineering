e206d1a0c3068add91e35bce2af9b93f1eb6b35b
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-30 16:23:18 +0200

    Add files via upload

---
+++ Programming Basics/Exams/05.Exam/Exam/05.BestPlayer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.BestPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int max = int.MinValue;
+            string bestPlayer = "";
+            while (name != "END")
+            {
+                int goalCount = int.Parse(Console.ReadLine());
+                if (goalCount > max)
+                {
+                    max = goalCount;
+                    bestPlayer = name;
+                }
+                if (goalCount >= 10)
+                {
+                    break;
+                }
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"{bestPlayer} is the best player!");
+            if (max >= 3)
+            {
+                Console.WriteLine($"He has scored {max} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {max} goals.");
+            }
+        }
+    }
+}
