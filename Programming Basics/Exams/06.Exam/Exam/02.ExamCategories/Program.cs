987c3cdeba992ffbf251dbc4140167e313fd73b7
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-31 17:20:15 +0200

    Add files via upload

---
+++ Programming Basics/Exams/06.Exam/Exam/02.ExamCategories/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02.ExamCategories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int complexity = int.Parse(Console.ReadLine());
+            int spinning = int.Parse(Console.ReadLine());
+            int pages = int.Parse(Console.ReadLine());
+            if (complexity >= 80 && spinning >= 80 && pages >= 8)
+            {
+                Console.WriteLine("Legacy");
+            }
+            if (complexity >= 80 && spinning <= 10)
+            {
+                Console.WriteLine("Master");
+            }
+            if (spinning >= 50 && pages >= 2)
+            {
+                Console.WriteLine("Hard");
+            }
+            if (complexity <= 30 && pages <= 1)
+            {
+                Console.WriteLine("Easy");
+            }
+            if (complexity <= 10)
+            {
+                Console.WriteLine("Elementary");
+            }
+            else
+            {
+                Console.WriteLine("Regular");
+            }
+        }
+    }
+}
