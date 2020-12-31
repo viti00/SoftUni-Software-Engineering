27755e62179a18ae0f035eeb05d34ddfef9dbc30
viti00 <75797230+viti00@users.noreply.github.com>
2020-12-31 17:16:12 +0200

    Add files via upload

---
+++ Programming Basics/Exams/06.Exam/Exam/01.ExamSubmission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.ExamSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentCount = int.Parse(Console.ReadLine());
+            int taskCount = int.Parse(Console.ReadLine());
+            double submissions = taskCount * 2.8;
+            double total = studentCount * Math.Ceiling(submissions);
+            double bonusSSubmissions = studentCount * (taskCount / 3);
+            double totalSubmissions = bonusSSubmissions + total;
+            double needed = totalSubmissions / 10;
+            double totalNeeded = 5 * Math.Ceiling(needed);
+            Console.WriteLine($"{totalNeeded} KB needed");
+            Console.WriteLine($"{totalSubmissions} submissions");
+        }
+    }
+}
