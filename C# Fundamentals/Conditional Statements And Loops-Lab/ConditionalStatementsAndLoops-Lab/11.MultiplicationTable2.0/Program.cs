da2a0bf541c5f3d9b07e9e2a1c37ec0273e29e96
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-02 13:12:09 +0200

    Add files via upload

---
+++ C# Fundamentals/Conditional Statements And Loops-Lab/ConditionalStatementsAndLoops-Lab/11.MultiplicationTable2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _11.MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int multiplication = 0;
+            for (int i = num2; i <= 10; i++)
+            {
+                multiplication = num1 * i;
+                Console.WriteLine($"{num1} X {i} = {multiplication}");
+            }
+            if (num2 > 10)
+            {
+                multiplication = num1 * num2;
+                Console.WriteLine($"{num1} X {num2} = {multiplication}");
+            }
+        }
+    }
+}
