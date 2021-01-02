69291d08859b78c5be517e3b45a308a4260a44f2
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-02 13:12:41 +0200

    Add files via upload

---
+++ C# Fundamentals/Conditional Statements And Loops-Lab/ConditionalStatementsAndLoops-Lab/12.EvenNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _12.EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            while (num % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                num = int.Parse(Console.ReadLine());
+            }
+            if (num % 2 == 0)
+            {
+                if (num < 0)
+                {
+                    int num2 = num * (-1);
+                    Console.WriteLine($"The number is: {num2}");
+                }
+                else
+                {
+                    Console.WriteLine($"The number is: {num}");
+                }
+            }
+        }
+    }
+}
