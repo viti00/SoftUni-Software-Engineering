10a331038eab6fa871bc9b228d6ec736cc4a1bfb
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-02 13:11:06 +0200

    Add files via upload

---
+++ C# Fundamentals/Conditional Statements And Loops-Lab/ConditionalStatementsAndLoops-Lab/09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09.SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int nSum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("{0}", 2 * i - 1);
+                nSum += 2 * i - 1;
+            }
+            Console.WriteLine("Sum: {0}", nSum);
+        }
+    }
+}
