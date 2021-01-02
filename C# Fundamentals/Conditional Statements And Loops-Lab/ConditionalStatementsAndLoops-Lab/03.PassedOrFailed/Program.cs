009e61d76565559afe056133e177ce63ea1f66a1
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-02 12:59:55 +0200

    Add files via upload

---
+++ C# Fundamentals/Conditional Statements And Loops-Lab/ConditionalStatementsAndLoops-Lab/03.PassedOrFailed/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.PassedOrFailed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else if (grade < 3.00)
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
