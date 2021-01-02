0b39ff9efd41ac27c529235cc75f656fc15338b2
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-02 13:08:48 +0200

    Add files via upload

---
+++ C# Fundamentals/Conditional Statements And Loops-Lab/ConditionalStatementsAndLoops-Lab/04.BackIn30Minutes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04.BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int afrer30Min = minutes + 30;
+            if (afrer30Min > 59)
+            {
+                hours++;
+                minutes = afrer30Min - 60;
+            }
+            else
+            {
+                minutes = afrer30Min;
+            }
+            if (hours > 23)
+            {
+                hours = 24 - hours;
+            }
+            if (minutes < 10)
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+        }
+    }
+}
