4d3d2f33bf45d620c3e01a118a689f5fcaf0eff2
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-04 12:43:02 +0200

    Add files via upload

---
+++ C# Fundamentals/Conditional Statements And Loops-Lab/Conditional Statements and Loops - Exercise/ConditionalStatementsAndLoops-Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06.StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int total = 0;
+            foreach (char ch in num)
+            {
+                int number = ch - 48;
+                int factorial = 1;
+                for (var j = 1; j <= number; j++)
+                {
+                    factorial *= j;
+                }
+                total += factorial;
+            }
+            int stringToNum = int.Parse(num);
+            if (stringToNum == total)
+            {
+                Console.WriteLine("yes");
+            }
+            else if (stringToNum != total)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
