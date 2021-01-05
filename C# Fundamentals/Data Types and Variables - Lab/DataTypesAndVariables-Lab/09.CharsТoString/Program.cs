5270544ace0a319f4ccbc0e886d2ef4dca7807d0
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-05 13:02:39 +0200

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables - Lab/DataTypesAndVariables-Lab/09.CharsТoString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _09.CharsТoString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}{b}{c}");
+        }
+    }
+}
