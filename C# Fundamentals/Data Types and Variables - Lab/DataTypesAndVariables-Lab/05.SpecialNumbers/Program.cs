15f73e192ac444cedd9255db7ee80aed72d66ca8
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-05 13:00:01 +0200

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables - Lab/DataTypesAndVariables-Lab/05.SpecialNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int num = 1; num <= n; num++)
+            {
+                int sumOfDigits = 0;
+                int digit = num;
+                while (digit > 0)
+                {
+                    sumOfDigits += digit % 10;
+                    digit = digit / 10;
+                }
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{num} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} -> False");
+                }
+            }
+        }
+    }
+}
