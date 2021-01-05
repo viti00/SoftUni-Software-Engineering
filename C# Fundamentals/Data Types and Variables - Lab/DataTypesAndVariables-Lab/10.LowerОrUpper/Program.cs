b9e07b24d1e1f58bb90ccc5fdd4c377f30a6bf17
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-05 13:03:09 +0200

    Add files via upload

---
+++ C# Fundamentals/Data Types and Variables - Lab/DataTypesAndVariables-Lab/10.LowerОrUpper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10.LowerОrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char value = char.Parse(Console.ReadLine());
+            int charValue = Convert.ToInt32(value);
+            if (charValue >= 65 && charValue <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (charValue >= 97 && charValue <= 122)
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
