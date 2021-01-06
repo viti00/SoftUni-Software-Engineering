ef440423f697a5e6d31e4a002f9a143294d64f19
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-06 12:49:51 +0200

    Add files via upload

---
+++ C# Fundamentals/DataTypesAndVariables-Exercise/10.PokeMon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10.PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            int absolutNvalue = N;
+            int targetCounter = 0;
+            int remained = 0;
+            while (N >= M)
+            {
+                N -= M;
+                remained = N;
+                targetCounter++;
+                if (remained == absolutNvalue * 0.5)
+                {
+                    if (Y > 0)
+                    {
+                        N /= Y;
+                        remained = N;
+                    }
+                }
+            }
+            if (remained < M)
+            {
+                Console.WriteLine(remained);
+                Console.WriteLine(targetCounter);
+            }
+        }
+    }
+}
