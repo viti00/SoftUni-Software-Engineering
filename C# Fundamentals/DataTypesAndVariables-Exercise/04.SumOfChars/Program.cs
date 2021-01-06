af436f6000bb6d431a13ebe09830dd9f6be0ad4e
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-06 12:45:18 +0200

    Add files via upload

---
+++ C# Fundamentals/DataTypesAndVariables-Exercise/04.SumOfChars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.SumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumValue = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                char value = char.Parse(Console.ReadLine());
+                int charValue = Convert.ToInt16(value);
+                sumValue += charValue;
+            }
+            Console.WriteLine($"The sum equals: {sumValue}");
+        }
+    }
+}
