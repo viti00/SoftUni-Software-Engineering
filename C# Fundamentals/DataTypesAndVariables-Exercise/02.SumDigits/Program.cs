045f732c650a5b00fdd976c248fee997f317bc14
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-06 12:44:17 +0200

    Add files via upload

---
+++ C# Fundamentals/DataTypesAndVariables-Exercise/02.SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int digitSum = 0;
+            int digit = num;
+            while (digit > 0)
+            {
+                digitSum += digit % 10;
+                digit = digit / 10;
+            }
+            Console.WriteLine(digitSum);
+        }
+    }
+}
