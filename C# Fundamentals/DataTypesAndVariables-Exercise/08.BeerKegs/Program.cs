d65e5caec2f3c8e4bf05477f90fa1c41772aad50
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-06 12:48:34 +0200

    Add files via upload

---
+++ C# Fundamentals/DataTypesAndVariables-Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double volumeOfKeg = 0.0;
+            double max = double.MinValue;
+            string biggestKeg = string.Empty;
+            for (int i = 1; i <= n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                volumeOfKeg = Math.PI * (radius * radius) * height;
+                if (volumeOfKeg > max)
+                {
+                    max = volumeOfKeg;
+                    biggestKeg = model;
+                }
+            }
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
