52debbcc0f3820f06da98417d5c99af7f57ede21
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-06 12:46:39 +0200

    Add files via upload

---
+++ C# Fundamentals/DataTypesAndVariables-Exercise/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06.TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 97; i < 97 + n; i++)
+            {
+                for (int j = 97; j < 97 + n; j++)
+                {
+                    for (int g = 97; g < 97 + n; g++)
+                    {
+                        Console.WriteLine($"{Convert.ToChar(i)}{Convert.ToChar(j)}{Convert.ToChar(g)}");
+                    }
+                }
+            }
+        }
+    }
+}
