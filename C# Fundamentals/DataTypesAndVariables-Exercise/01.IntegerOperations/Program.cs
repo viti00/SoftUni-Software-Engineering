53b02712f98e9553917df80f4a86f6b4ccdf59d0
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-06 12:35:11 +0200

    Add files via upload

---
+++ C# Fundamentals/DataTypesAndVariables-Exercise/01.IntegerOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+            int sum = firstNum + secondNum;
+            double divide = sum / thirdNum;
+            double multiply = divide * fourthNum;
+            Console.WriteLine(multiply);
+        }
+    }
+}
