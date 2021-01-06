9e3c1d88aef45cdf321b86d24f128df5298beef9
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-06 12:49:04 +0200

    Add files via upload

---
+++ C# Fundamentals/DataTypesAndVariables-Exercise/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _09.SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sumSpice = 0;
+            int yield = num;
+            int dayCounter = 0;
+            int totalAmount = 0;
+            while (yield >= 100)
+            {
+                sumSpice += yield;
+                sumSpice -= 26;
+                yield -= 10;
+                dayCounter++;
+            }
+            if (yield < 100)
+            {
+                if (sumSpice < 26)
+                {
+                    totalAmount = sumSpice;
+                }
+                else
+                {
+                    sumSpice -= 26;
+                    totalAmount = sumSpice;
+                }
+            }
+            Console.WriteLine(dayCounter);
+            Console.WriteLine(totalAmount);
+        }
+    }
+}
