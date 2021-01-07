793ed41aa59b3540d4d35a8402dca1174f23d1bc
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-07 12:54:03 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Lab/02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                arr[i] = number;
+            }
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
