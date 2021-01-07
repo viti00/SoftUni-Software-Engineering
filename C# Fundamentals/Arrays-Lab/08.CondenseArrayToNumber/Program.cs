b9472992690b42545cc64d858744a2133e1a9dc4
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-07 12:58:43 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Lab/08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08.CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] nums = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                nums[i] = int.Parse(input[i]);
+            }
+            while (nums.Length != 1)
+            {
+                int[] newArr = new int[nums.Length - 1];
+                for (int i = 0; i < newArr.Length; i++)
+                {
+                    newArr[i] = nums[i] + nums[i + 1];
+                }
+                nums = newArr;
+            }
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
