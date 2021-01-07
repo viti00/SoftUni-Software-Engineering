59b0ec657ceb18f348a439691b25c03fd919a7ab
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-07 12:55:17 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Lab/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04.ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words = Console.ReadLine();
+            string[] splited = words.Split();
+            for (int i = splited.Length - 1; i >= 0; i--)
+            {
+                Console.Write(splited[i] + " ");
+            }
+        }
+    }
+}
