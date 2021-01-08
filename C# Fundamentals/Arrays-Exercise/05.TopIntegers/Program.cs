b8df746cd5a5c87550a374620e46d735e149ba68
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-08 12:56:28 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Exercise/05.TopIntegers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            bool isBigger = true;
+            for (int i = 0; i < values.Length; i++)
+            {
+                int currentElement = values[i];
+                for (int j = i + 1; j < values.Length; j++)
+                {
+                    if (values[j] >= currentElement)
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                if (isBigger)
+                {
+                    Console.Write(currentElement + " ");
+                }
+                isBigger = true;
+            }
+        }
+    }
+}
