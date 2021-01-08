7e87ac2fcbc82b53c5709484a9d736d2e6c438a0
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-08 12:47:35 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Exercise/01.Train/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int peoples = int.Parse(Console.ReadLine());
+                arr[i] = peoples;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            int sum = arr.Sum();
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
