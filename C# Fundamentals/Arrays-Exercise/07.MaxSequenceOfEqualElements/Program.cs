44a5e92fe6318805e0a2e78f5b5da4df2fc76b2c
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-08 12:57:30 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Exercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                                 .Split();
+            int bestCount = 0;
+            int bestIndex = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string currentElement = arr[i];
+                int currentCounter = 1;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (currentElement == arr[j])
+                    {
+                        currentCounter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (currentCounter > bestCount)
+                {
+                    bestCount = currentCounter;
+                    bestIndex = i;
+                }
+
+            }
+            for (int i = 0; i < bestCount; i++)
+            {
+                Console.Write(arr[bestIndex] + " ");
+            }
+        }
+    }
+}
