5e65631680a266e93cb2bbd02cfcd23f9acff2ac
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-08 12:55:54 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Exercise/04.ArrayRotation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine()
+                                     .Split()
+                                     .ToArray();
+            // n is number of rotation (first number goes at the end)
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string elementToRotate = values[0];
+
+                for (int j = 1; j < values.Length; j++)
+                {
+                    string curentElemnt = values[j];
+                    values[j - 1] = curentElemnt;
+                }
+                values[values.Length - 1] = elementToRotate;
+            }
+            Console.WriteLine(string.Join(" ", values));
+        }
+    }
+}
