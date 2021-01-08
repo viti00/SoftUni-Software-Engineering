d2f737e08020402b02dddeb3ae4a85a3e9b4bc13
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-08 12:58:36 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int firstNumber = numbers[i];
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int seconNumber = numbers[j];
+                    if (firstNumber + seconNumber == num)
+                    {
+                        Console.WriteLine($"{firstNumber} {seconNumber}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
