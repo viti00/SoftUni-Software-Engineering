9eef09bccdcb139700fb81521419a9e2b2171d40
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-08 12:56:55 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Exercise/06.EqualSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EqualSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+            bool isFound = false;
+            for (int curr = 0; curr < arr.Length; curr++)
+            {
+                int rightSum = 0;
+
+                for (int i = curr + 1; i < arr.Length; i++)
+                {
+                    rightSum += arr[i];
+                }
+
+                int leftSum = 0;
+
+                for (int i = curr - 1; i >= 0; i--)
+                {
+                    leftSum += arr[i];
+                }
+
+                if (rightSum == leftSum)
+                {
+                    Console.WriteLine(curr);
+                    isFound = true;
+                    break;
+                }
+            }
+
+            if (isFound == false)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
