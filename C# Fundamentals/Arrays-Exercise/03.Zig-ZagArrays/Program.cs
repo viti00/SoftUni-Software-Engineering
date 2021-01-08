1c5ce4e6b907c8b155a85e84449fe349f8d7b3af
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-08 12:55:20 +0200

    Add files via upload

---
+++ C# Fundamentals/Arrays-Exercise/03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Zig_ZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] firstArr = new string[n];
+            string[] secondArr = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentArray = Console.ReadLine()
+                                               .Split(" ")
+                                               .ToArray();
+                string indexZeroElement = currentArray[0];
+                string indexOneElement = currentArray[1];
+
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = indexZeroElement;
+                    secondArr[i] = indexOneElement;
+                }
+                else if (i % 2 == 1)
+                {
+                    firstArr[i] = indexOneElement;
+                    secondArr[i] = indexZeroElement;
+                }
+            }
+            Console.Write(string.Join(" ", firstArr));
+            Console.WriteLine();
+            Console.Write(string.Join(" ", secondArr));
+        }
+    }
+}
