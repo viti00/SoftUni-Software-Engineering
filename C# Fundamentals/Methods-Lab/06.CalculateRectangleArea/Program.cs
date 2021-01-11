b1d935ff081e763fc892c9a8f0bca92affd5e370
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-11 12:55:41 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06.CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine(PrintArea(width, height));
+        }
+        static double PrintArea(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
