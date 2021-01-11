c5316601fd61eaa6095737eaca0d1d64dff924cb
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-11 12:58:33 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Lab/11.MathOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _11.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            double num2 = double.Parse(Console.ReadLine());
+            double result = Calculator(num1, @operator, num2);
+            Console.WriteLine(Math.Round(result, 2));
+        }
+        static double Calculator(double num1, string @operator, double num2)
+        {
+            double result = 0;
+            switch (@operator)
+            {
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+            }
+            return result;
+        }
+    }
+}
