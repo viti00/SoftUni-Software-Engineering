7278f056f986ec85b78be01d424f30c6af486371
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-11 12:54:07 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Lab/03.Calculations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _03.Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            switch (command)
+            {
+                case "add":
+                    PrintAdd(num1, num2);
+                    break;
+                case "multiply":
+                    PrintMultiply(num1, num2);
+                    break;
+                case "subtract":
+                    PrintSubtract(num1, num2);
+                    break;
+                case "divide":
+                    PrintDivide(num1, num2);
+                    break;
+            }
+        }
+        static void PrintAdd(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+        static void PrintMultiply(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+        static void PrintSubtract(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+        static void PrintDivide(int num1, int num2)
+        {
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
