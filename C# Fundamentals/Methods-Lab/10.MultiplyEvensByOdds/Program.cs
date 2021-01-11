7296b914f6d38dde6ba166c0e490c47e4f20fbb1
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-11 12:57:47 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Lab/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _10.MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+        }
+        static int GetMultipleOfEvenAndOdds(int num)
+        {
+            int multiplySum = GetSumOfEvenDigits(num) * GetSumOfOddDigits(num);
+            return multiplySum;
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int evenSum = 0;
+            int digit = number;
+            while (digit > 0)
+            {
+                int digitFromNumber = number % 10;
+                digit = digit / 10;
+                if (digitFromNumber % 2 == 0)
+                {
+                    evenSum += digitFromNumber;
+                }
+                number = digit;
+            }
+            return evenSum;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int digit = number;
+            int oddSum = 0;
+            while (digit > 0)
+            {
+                int digitFromNumber = number % 10;
+                digit = digit / 10;
+                if (digitFromNumber % 2 == 1)
+                {
+                    oddSum += digitFromNumber;
+                }
+                number = digit;
+            }
+            return oddSum;
+        }
+    }
+}
