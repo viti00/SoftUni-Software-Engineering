12d6ce1daa955271a6d6fe14228bcefae5aa5a9b
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-11 12:56:43 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Lab/08.MathPower/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            Console.WriteLine(RiseToPower(num, power));
+        }
+        static double RiseToPower(double num, int pow)
+        {
+            double result = 0d;
+            result = Math.Pow(num, pow);
+            return result;
+        }
+    }
+}
