5380711b8b83a081159e67b1d139697deac8eade
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-11 12:56:11 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Lab/07.RepeatString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(RepeatString(word, n));
+        }
+        private static string RepeatString(string word, int counter)
+        {
+            string result = " ";
+            for (int i = 1; i <= counter; i++)
+            {
+                result += word;
+            }
+            return result;
+        }
+    }
+}
