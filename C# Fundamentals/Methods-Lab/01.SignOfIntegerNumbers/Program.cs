4de24d5e1c7c60d0a91701436e4ff2142d67151f
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-11 12:44:42 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Lab/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            PrintSign(num);
+        }
+        static void PrintSign(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Methods-Lab/02.Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            PrintGradeType(grade);
+        }
+        static void PrintGradeType(double n)
+        {
+            if (n >= 2.00 && n <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (n >= 3.00 && n <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (n >= 3.50 && n <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (n >= 4.50 && n <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (n >= 5.50 && n <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
