6cc5692a9f9052e8d1c77bfa4d13578f297562ad
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-12 12:44:46 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Exercise/10.TopNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _10.TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            TopNumber(num);
+        }
+        static void TopNumber(int n)
+        {
+            int digitSum = 0;
+
+            for (int curr = 17; curr <= n; curr++)
+            {
+                int number = curr;
+                int digit = curr;
+                while (digit > 0)
+                {
+                    digitSum += digit % 10;
+                    int currDigit = digit % 10;
+                    digit = digit / 10;
+                    if (currDigit % 2 == 1)
+                    {
+                        if (digitSum % 8 == 0)
+                        {
+                            Console.WriteLine(number);
+                        }
+                    }
+                }
+                digitSum = 0;
+            }
+        }
+    }
+}
