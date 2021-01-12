82383b84a2448a5bc72ca5a3dc2f4482ae716104
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-12 12:42:40 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06.MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(PrintMiddle(word));
+        }
+        static string PrintMiddle(string word)
+        {
+            int lenght = word.Length;
+            string result = string.Empty;
+            if (lenght % 2 == 1)
+            {
+                result = word[word.Length / 2].ToString();
+            }
+            else
+            {
+                result = word[word.Length / 2 - 1].ToString() + word[word.Length / 2].ToString();
+            }
+            return result;
+        }
+    }
+}
