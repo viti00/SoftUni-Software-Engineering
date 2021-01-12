28b98efd13a53f03354d46f3b64f9f03f2528293
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-12 12:40:53 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Exercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03.CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            PrintCharInRange(first, second);
+        }
+        static void PrintCharInRange(char first, char second)
+        {
+
+            if (first < second)
+            {
+                for (int i = first + 1; i < second; i++)
+                {
+                    char result = (char)i;
+                    Console.Write(result + " ");
+                }
+            }
+            else if (first > second)
+            {
+                for (int i = second + 1; i < first; i++)
+                {
+                    char result = (char)i;
+                    Console.Write(result + " ");
+                }
+            }
+
+        }
+    }
+}
