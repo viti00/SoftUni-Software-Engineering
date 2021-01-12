689bcda47b934e23fb6a31afc1bdcae3d9640066
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-12 12:31:13 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Exercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintSmallest(num1, num2, num3));
+        }
+        static int PrintSmallest(int num1, int num2, int num3)
+        {
+            int result = 0;
+            if (num1 < num2 && num1 < num3)
+            {
+                result = num1;
+            }
+            else if (num2 < num1 && num2 < num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+            return result;
+        }
+    }
+}
