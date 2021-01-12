031d7aa2b635fd90dc2e58f56699b4ac9b439be8
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-12 12:43:19 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Exercise/07.NxNMatrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07.NxNMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);
+        }
+        static void PrintMatrix(int n)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
