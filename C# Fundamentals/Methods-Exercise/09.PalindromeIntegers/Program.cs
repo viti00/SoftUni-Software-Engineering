8b2fb2f7f0c5c0e690d6daaa8c042a56e20118f3
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-12 12:44:18 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            PalindromeInteger(number);
+        }
+        static void PalindromeInteger(string number)
+        {
+            string result = string.Empty;
+            char[] sRev = number.ToArray();
+            string reversNumber = string.Empty;
+            while (number != "End")
+            {
+
+                string inputNumber = number;
+                for (int i = sRev.Length - 1; i >= 0; i--)
+                {
+                    reversNumber += sRev[i];
+                }
+                if (inputNumber == reversNumber)
+                {
+                    result = "true";
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    result = "false";
+                    Console.WriteLine(result);
+                }
+                number = Console.ReadLine();
+            }
+        }
+    }
+}
