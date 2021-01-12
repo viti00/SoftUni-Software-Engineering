54ea52d2878d2209a4c72353c98e3c8fff790516
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-12 12:40:20 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            VowelsCount(word);
+
+        }
+        static void VowelsCount(string word)
+        {
+            char[] letter = word.ToArray();
+            int counter = 0;
+            foreach (char item in letter)
+            {
+                if (item == 'a' || item == 'o' || item == 'u' || item == 'e' || item == 'i'
+                    || item == 'A' || item == 'O' || item == 'U' || item == 'E' || item == 'I')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
