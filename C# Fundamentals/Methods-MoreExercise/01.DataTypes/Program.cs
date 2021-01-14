770770deea915affe7df1b8719deea113eabca15
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-14 12:31:09 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-MoreExercise/01.DataTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01.DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            if (type == "int")
+            {
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(ChekTypes(num));
+            }
+            else if (type == "real")
+            {
+                double num = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{ChekTypes(num):f2}");
+            }
+            else if (type == "string")
+            {
+                string word = Console.ReadLine();
+                Console.WriteLine(ChekTypes(word));
+            }
+        }
+        static int ChekTypes(int num)
+        {
+            int result = num * 2;
+            return result;
+        }
+        static double ChekTypes(double num)
+        {
+            double result = num * 1.5;
+            return result;
+        }
+        static string ChekTypes(string word)
+        {
+            string result = $"${word}$";
+            return result;
+        }
+    }
+}
