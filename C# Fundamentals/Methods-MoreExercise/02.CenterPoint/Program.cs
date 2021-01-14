a2e6d19a879f4bf15d81371496a16e6aaabf70a8
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-14 12:36:55 +0200

    Add files via upload

---
+++ C# Fundamentals/Methods-MoreExercise/02.CenterPoint/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.CenterPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            PrintClosestPointsToTheCenter(x1, y1, x2, y2);
+        }
+        static void PrintClosestPointsToTheCenter(double x1, double y1, double x2, double y2)
+        {
+            if (Math.Abs(x1) + Math.Abs(y1) < Math.Abs(x2) + Math.Abs(y2))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else if (Math.Abs(x1) + Math.Abs(y1) > Math.Abs(x2) + Math.Abs(y2))
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+        }
+    }
+}
