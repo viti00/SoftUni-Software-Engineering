0d5f4098016eb940b47c1f104098bb38e6e43b89
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-16 13:41:24 +0200

    Add files via upload

---
+++ C# Fundamentals/List-Lab/02.Gauss'Trick/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Gauss_Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = SingleLine();
+            Console.WriteLine(string.Join(" ", SumPairs(list)));
+        }
+        static List<int> SumPairs(List<int> number)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < number.Count / 2; i++)
+            {
+                result.Add(number[i] + number[number.Count - i - 1]);
+            }
+            if (number.Count % 2 == 1)
+            {
+                result.Add(number[number.Count / 2]);
+            }
+            return result;
+        }
+        static List<int> SingleLine()
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            return list;
+        }
+    }
+}
