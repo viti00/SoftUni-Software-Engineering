e0d39e463ea59f5e6f410d15b8288e51363f4d83
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-16 13:43:11 +0200

    Add files via upload

---
+++ C# Fundamentals/List-Lab/04.ListOfProducts/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string currProduct = Console.ReadLine();
+                products.Add(currProduct);
+            }
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
