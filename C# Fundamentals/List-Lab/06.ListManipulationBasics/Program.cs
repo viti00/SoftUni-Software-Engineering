3c4b144b3c6032ccaff7ff6d5c8192deab3f0902
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-16 13:44:30 +0200

    Add files via upload

---
+++ C# Fundamentals/List-Lab/06.ListManipulationBasics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6.ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+                string[] tokens = line.Split();
+                switch (tokens[0])
+                {
+                    case "Add":
+                        int NumberToAdd = int.Parse(tokens[1]);
+                        numbers.Add(NumberToAdd);
+                        break;
+                    case "Remove":
+                        int NumberToRemove = int.Parse(tokens[1]);
+                        numbers.Remove(NumberToRemove);
+                        break;
+                    case "RemoveAt":
+                        int indexToRemove = int.Parse(tokens[1]);
+                        numbers.RemoveAt(indexToRemove);
+                        break;
+                    case "Insert":
+                        int NumberToInsert = int.Parse(tokens[1]);
+                        int IndexToInsert = int.Parse(tokens[2]);
+                        numbers.Insert(IndexToInsert, NumberToInsert);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
