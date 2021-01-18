128e32c231f075e93f9711442299b3c01043d32b
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-18 12:42:50 +0200

    Add files via upload

---
+++ C# Fundamentals/List-Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> array = Console.ReadLine()
+                .Split('|')
+                .Reverse()
+                .ToList();
+            List<int> numbers = new List<int>();
+            foreach (var str in array)
+            {
+                numbers.AddRange(str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList()
+                    );
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
