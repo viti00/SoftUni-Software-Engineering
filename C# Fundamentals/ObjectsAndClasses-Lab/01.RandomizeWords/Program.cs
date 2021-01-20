189caf0e3b73f131fea1edc8b180ddeae955844f
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-20 12:26:54 +0200

    Add files via upload

---
+++ C# Fundamentals/ObjectsAndClasses-Lab/01.RandomizeWords/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1.RandomizeWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            stringRandomizer randomizer = new stringRandomizer();
+            randomizer.Words = Console.ReadLine().Split();
+            randomizer.Randomize();
+            randomizer.PrintRnd();
+        }
+        public class stringRandomizer
+        {
+            public string[] Words;
+            public void Randomize()
+            {
+                Random rand = new Random();
+                for (int i = 0; i < Words.Length; i++)
+                {
+                    int randomPosition = rand.Next(0, Words.Length);
+                    string temp = Words[i];
+                    Words[i] = Words[randomPosition];
+                    Words[randomPosition] = temp;
+                }
+            }
+
+            public void PrintRnd()
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, Words));
+            }
+        }
+    }
+}
