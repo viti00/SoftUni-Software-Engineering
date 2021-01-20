3ff1b62709aa5bad5149e449ce3c04d67922de98
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-20 12:33:08 +0200

    Add files via upload

---
+++ C# Fundamentals/ObjectsAndClasses-Lab/03.Songs/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+            for (int i = 1; i <= n; i++)
+            {
+                string[] temp = Console.ReadLine().Split("_");
+                string type = temp[0];
+                string name = temp[1];
+                string time = temp[2];
+
+                Song song = new Song();
+                song.TypeList = type;
+                song.Name = name;
+                song.Time = time;
+
+                songs.Add(song);
+
+            }
+            string listType = Console.ReadLine();
+            if (listType == "all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList == listType)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+
+        }
+        public class Song
+        {
+            public string TypeList { get; set; }
+            public string Name { get; set; }
+            public string Time { get; set; }
+        }
+
+    }
+}
