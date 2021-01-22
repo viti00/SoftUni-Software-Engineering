dfd311f212119a626eb3334c07e7292236854817
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-22 14:21:45 +0200

    Add files via upload

---
+++ C# Fundamentals/ObjectsAndClasses-Exercise/01.AdvertisementMessage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.AdvertisementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrases =
+                { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+            string[] events =
+                { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] authors =
+                { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+            int n = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            for (int i = 1; i <= n; i++)
+            {
+                int randomPhrases = rnd.Next(0, phrases.Length);
+                int randomEvents = rnd.Next(0, events.Length);
+                int randomAuthors = rnd.Next(0, authors.Length);
+                int randomCities = rnd.Next(0, cities.Length);
+                Console.WriteLine($"{phrases[randomPhrases]} {events[randomEvents]} {authors[randomAuthors]} - {cities[randomCities]}.");
+            }
+
+        }
+    }
+}
