c7ef589ef1860ce1e021e4b7608b2c0a274256b3
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-25 12:50:12 +0200

    Add files via upload

---
+++ C# Fundamentals/MidExams/04.NationalCourt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NationalCourt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmployee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+            int totalAnswerForHour = firstEmployee + secondEmployee + thirdEmployee;
+            int hourCount = 0;
+            while (peopleCount > 0)
+            {
+
+                hourCount++;
+                if (hourCount % 4 == 0)
+                {
+                    continue;
+                }
+                peopleCount -= totalAnswerForHour;
+            }
+            Console.WriteLine($"Time needed: {hourCount}h.");
+        }
+    }
+}
