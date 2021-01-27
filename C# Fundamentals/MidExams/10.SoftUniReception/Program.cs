1bcbc6e912a2affa7d79c6a0bebb095d802db099
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-27 23:13:58 +0200

    Add files via upload

---
+++ C# Fundamentals/MidExams/10.SoftUniReception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeCapacity = int.Parse(Console.ReadLine());
+            int secondEmployeeCapacity = int.Parse(Console.ReadLine());
+            int thirdEmployeeCapacity = int.Parse(Console.ReadLine());
+            int questions = int.Parse(Console.ReadLine());
+            int hourCounter = 0;
+            int totalCapacity = firstEmployeeCapacity + secondEmployeeCapacity + thirdEmployeeCapacity;
+            while (questions > 0)
+            {
+
+                hourCounter++;
+                if (hourCounter % 4 == 0)
+                {
+                    continue;
+                }
+                questions -= totalCapacity;
+
+            }
+            Console.WriteLine($"Time needed: {hourCounter}h.");
+        }
+    }
+}
