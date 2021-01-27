14a463807121b21956af76132786d294a8c5409b
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-27 23:12:16 +0200

    Add files via upload

---
+++ C# Fundamentals/MidExams/07.Counter Strike/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CountreStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int wonCounter = 0;
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(command);
+                if (energy < distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wonCounter} won battles and {energy} energy");
+                    return;
+                }
+                wonCounter++;
+                energy -= distance;
+                if (wonCounter % 3 == 0)
+                {
+                    energy += wonCounter;
+                }
+            }
+            Console.WriteLine($"Won battles: {wonCounter}. Energy left: {energy}");
+        }
+    }
+}
