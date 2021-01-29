be55cf4cbaf6058d719a5eed6ddeb15c82ca1006
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-29 12:30:27 +0200

    Add files via upload

---
+++ C# Fundamentals/AssociativeArrays-Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+            for (int i = 1; i <= n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (words.ContainsKey(word) == false)
+                {
+                    words.Add(word, new List<string>());
+                    words[word].Add(synonym);
+                }
+                else
+                {
+                    words[word].Add(synonym);
+                }
+            }
+            foreach (var item in words)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
