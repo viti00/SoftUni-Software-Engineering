51493e7807359ae599b94136a7061fc770ae7515
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-29 12:24:39 +0200

    Add files via upload

---
+++ C# Fundamentals/AssociativeArrays-Lab/01.CountRealNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+            foreach (var number in numbers)
+            {
+                if (count.ContainsKey(number))
+                {
+                    count[number]++;
+                }
+                else
+                {
+                    count.Add(number, 1);
+                }
+            }
+            foreach (var number in count)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
