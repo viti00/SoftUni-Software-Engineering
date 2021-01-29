6cc9ce26576d2d7fd5ba3dceca10f152cda6093c
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-29 12:32:50 +0200

    Add files via upload

---
+++ C# Fundamentals/AssociativeArrays-Lab/05.WordFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .Where(w => w.Length % 2 == 0)
+                .ToArray();
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
