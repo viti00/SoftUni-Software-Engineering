14e9c4af46f4cc3d5debc98326c2f022faf6a213
viti00 <75797230+viti00@users.noreply.github.com>
2021-01-29 12:31:36 +0200

    Add files via upload

---
+++ C# Fundamentals/AssociativeArrays-Lab/04.Largest3Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] sorted = numbers.OrderByDescending(n => n).ToArray();
+            if (sorted.Length > 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{sorted[i]}" + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", sorted));
+            }
+        }
+    }
+}
