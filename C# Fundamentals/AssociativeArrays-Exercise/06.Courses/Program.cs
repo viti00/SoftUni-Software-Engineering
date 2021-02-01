8344de41cd614ad878b2feb3e3558e8d044355ae
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-01 14:10:30 +0200

    Add files via upload

---
+++ C# Fundamentals/AssociativeArrays-Exercise/06.Courses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, List<string>> datas = new Dictionary<string, List<string>>();
+            while (command != "end")
+            {
+                string[] temp = command.Split(" : ");
+                string course = temp[0];
+                string name = temp[1];
+                if (datas.ContainsKey(course))
+                {
+                    datas[course].Add(name);
+                }
+                else
+                {
+                    datas.Add(course, new List<string> { name });
+                }
+                command = Console.ReadLine();
+            }
+            foreach (var currCourse in datas.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{currCourse.Key}: {currCourse.Value.Count}");
+                foreach (var item in currCourse.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
