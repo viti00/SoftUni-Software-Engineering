41a99d52e95374f29f20d58fe8c4f9fb4efd13d1
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-01 14:01:57 +0200

    Add files via upload

---
+++ C# Fundamentals/AssociativeArrays-Exercise/01.CountCharsInAString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<char, int> word = new Dictionary<char, int>();
+            foreach (var item in words)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    char n = item[i];
+                    if (word.ContainsKey(n))
+                    {
+                        word[n]++;
+                    }
+                    else
+                    {
+                        word.Add(n, 1);
+                    }
+                }
+            }
+            foreach (var item in word)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
