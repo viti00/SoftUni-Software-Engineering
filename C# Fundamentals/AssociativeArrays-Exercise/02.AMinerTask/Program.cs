53ff0d73f592e6b21718716ed6eb671613a84929
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-01 14:08:01 +0200

    Add files via upload

---
+++ C# Fundamentals/AssociativeArrays-Exercise/02.AMinerTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            long quantities = long.Parse(Console.ReadLine());
+            Dictionary<string, long> products = new Dictionary<string, long>();
+            while (true)
+            {
+                if (products.ContainsKey(product))
+                {
+                    products[product] += quantities;
+                }
+                else
+                {
+                    products.Add(product, quantities);
+                }
+                product = Console.ReadLine();
+                if (product == "stop")
+                {
+                    break;
+                }
+                quantities = long.Parse(Console.ReadLine());
+            }
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
