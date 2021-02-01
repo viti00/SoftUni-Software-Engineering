691bd73c62b774fe71331ca0f350ba7fb76e6ecb
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-01 14:11:30 +0200

    Add files via upload

---
+++ C# Fundamentals/AssociativeArrays-Exercise/08.CompanyUsers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, List<string>> datas = new Dictionary<string, List<string>>();
+            while (command != "End")
+            {
+                string[] temp = command.Split(" -> ");
+                string companyName = temp[0];
+                string employeeId = temp[1];
+                if (datas.ContainsKey(companyName))
+                {
+                    if (datas[companyName].Contains(employeeId))
+                    {
+
+                    }
+                    else
+                    {
+                        datas[companyName].Add(employeeId);
+                    }
+                }
+                else
+                {
+                    datas.Add(companyName, new List<string> { employeeId });
+                }
+                command = Console.ReadLine();
+            }
+            foreach (var curr in datas.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{curr.Key}");
+                foreach (var item in curr.Value)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
+
