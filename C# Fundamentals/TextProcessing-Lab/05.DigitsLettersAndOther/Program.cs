bd77b0044fd6c7c3db6b2f40288de0346b8ffdc3
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-08 14:48:49 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Lab/05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _5.Digit_letter_symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string digit = "";
+            string letter = "";
+            string otherSymbols = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    digit += text[i];
+                }
+                else if (char.IsLetter(text[i]))
+                {
+                    letter += text[i];
+                }
+                else
+                {
+                    otherSymbols += text[i];
+                }
+            }
+            Console.WriteLine($"{digit}");
+            Console.WriteLine($"{letter}");
+            Console.WriteLine($"{otherSymbols}");
+        }
+    }
+}
