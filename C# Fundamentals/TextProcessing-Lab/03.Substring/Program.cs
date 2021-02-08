ec74730bd0cbd6db6aba250d5f2a5de61a1708a5
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-08 14:47:00 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Lab/03.Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+            int index = text.IndexOf(word);
+            while (index > -1)
+            {
+                text = text.Remove(index, word.Length);
+                index = text.IndexOf(word);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
