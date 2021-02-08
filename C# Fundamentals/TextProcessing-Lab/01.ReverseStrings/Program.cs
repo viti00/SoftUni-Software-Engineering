1f7844fffe6c4675f032dc4f348ac18a46953375
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-08 14:42:55 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            string reverse = "";
+            while (command != "end")
+            {
+                for (int i = command.Length - 1; i >= 0; i--)
+                {
+                    reverse += command[i];
+                }
+                Console.WriteLine($"{command} = {reverse}");
+                reverse = "";
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
