51441f87ceef15f4b959c654480172b375696620
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-08 14:46:03 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Lab/02.RepeatStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringRepeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    Console.Write(words[i]);
+                }
+            }
+        }
+    }
+}
