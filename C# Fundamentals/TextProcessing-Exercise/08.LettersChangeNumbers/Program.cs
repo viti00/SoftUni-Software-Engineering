0e1a406a30e44464368e04a374d83fec5c40855d
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-11 12:47:57 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Exercise/08.LettersChangeNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sequence = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            double result = 0;
+            int position = 0;
+            double totalResult = 0;
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                string curr = sequence[i];
+                char firstLet = curr[0];
+                char secondLet = curr[curr.Length - 1];
+
+                double number = double.Parse(curr.Substring(1, curr.Length - 2));
+                if (char.IsUpper(firstLet))
+                {
+                    position = (int)firstLet - 64;
+                    result = number / position;
+                }
+                else
+                {
+                    position = (int)firstLet - 96;
+                    result = number * position;
+                }
+
+
+                if (char.IsUpper(secondLet))
+                {
+                    position = (int)secondLet - 64;
+                    result -= position;
+                }
+                else
+                {
+                    position = (int)secondLet - 96;
+                    result += position;
+                }
+
+                totalResult += result;
+                result = 0;
+            }
+            Console.WriteLine($"{totalResult:f2}");
+        }
+    }
+}
