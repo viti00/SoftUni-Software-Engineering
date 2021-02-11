4e8b856fb3c4254dfa4e504214fb357852c05687
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-11 12:38:01 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Exercise/01.ValidUsernames/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidUsername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+            string result = "";
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                if (usernames[i].Length >= 3 && usernames[i].Length <= 16)
+                {
+                    foreach (var ch in usernames[i])
+                    {
+                        if (Char.IsLetterOrDigit(ch) || ch == '-' || ch == '_')
+                        {
+                            result += ch;
+                            if (result.Length == usernames[i].Length)
+                            {
+                                Console.WriteLine(result);
+                            }
+                        }
+                    }
+                    result = "";
+                }
+            }
+        }
+    }
+}
