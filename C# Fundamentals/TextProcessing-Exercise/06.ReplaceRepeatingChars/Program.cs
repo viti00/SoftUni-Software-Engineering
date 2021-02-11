183be7f1861c33a00dc1020aa54854cd03dd0b18
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-11 12:46:26 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Exercise/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToList();
+            for (int i = 0; i < text.Count - 1; i++)
+            {
+                char curr = text[i];
+                char next = text[i + 1];
+                if (curr == next)
+                {
+                    text.RemoveAt(i);
+                    i--;
+                }
+            }
+            Console.WriteLine(string.Join("", text));
+        }
+    }
+}
