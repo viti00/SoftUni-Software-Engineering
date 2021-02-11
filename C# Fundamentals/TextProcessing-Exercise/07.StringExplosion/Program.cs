fc0108a54cad03043d5c92a07bffe7271a922260
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-11 12:46:55 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Exercise/07.StringExplosion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string field = Console.ReadLine();
+            int strenght = 0;
+            for (int i = 0; i < field.Length; i++)
+            {
+                var currChar = field[i];
+                if (currChar == '>')
+                {
+                    strenght += int.Parse(field[i + 1].ToString());
+                    continue;
+                }
+
+                if (strenght > 0)
+                {
+                    field = field.Remove(i, 1);
+                    i--;
+                    strenght--;
+                }
+            }
+            Console.WriteLine(field);
+        }
+    }
+}
