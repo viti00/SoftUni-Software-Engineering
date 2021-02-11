d6a59bbff1c5d5987bafb64145115e8a4f527d4e
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-11 12:45:20 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Exercise/04.CaesarCipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int crypt = 0;
+            string cryptedText = "";
+            foreach (var ch in text)
+            {
+                crypt = (int)ch + 3;
+                cryptedText += Convert.ToChar(crypt);
+            }
+            Console.WriteLine(cryptedText);
+        }
+    }
+}
