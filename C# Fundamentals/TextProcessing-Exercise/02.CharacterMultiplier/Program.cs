12c24e20a712ae35f481454e6176c5755f178642
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-11 12:43:56 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Exercise/02.CharacterMultiplier/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CharMultiply
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            string firstWord = words[0];
+            string secondWord = words[1];
+
+            string longestWord = firstWord;
+            string shortestWord = secondWord;
+
+            if (words[0].Length < words[1].Length)
+            {
+                longestWord = secondWord;
+                shortestWord = firstWord;
+            }
+            int total = CharMultiplier(longestWord, shortestWord);
+            Console.WriteLine(total);
+        }
+        public static int CharMultiplier(string longest, string shortest)
+        {
+            int sum = 0;
+            for (int i = 0; i < shortest.Length; i++)
+            {
+                int multiply = shortest[i] * longest[i];
+                sum += multiply;
+            }
+            for (int i = shortest.Length; i < longest.Length; i++)
+            {
+                sum += longest[i];
+            }
+            return sum;
+        }
+    }
+}
