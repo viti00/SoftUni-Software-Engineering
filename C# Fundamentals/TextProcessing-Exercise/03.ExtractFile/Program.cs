1abe01cd3c92621175c66cafd9cd4bd496830a4e
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-11 12:44:31 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-Exercise/03.ExtractFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Console.ReadLine();
+            int startIndexFileName = file.LastIndexOf("\\");
+            int endIndexFileName = file.IndexOf(".");
+            string fileName = file.Substring(startIndexFileName + 1, endIndexFileName - startIndexFileName - 1);
+            string filExtansion = file.Substring(endIndexFileName + 1);
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {filExtansion}");
+        }
+    }
+}
