004dfe5f5f9125ffd734e0a053f684a0d6104619
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-16 12:29:56 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-MoreExercise/02.AsciiSumator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AsciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+
+            int asciiCodeFirstSymbol = (int)firstSymbol;
+            int asciiCodeSecondSymbol = (int)secondSymbol;
+
+            string text = Console.ReadLine();
+            int sum = 0;
+            foreach (var ch in text)
+            {
+                if (ch > asciiCodeFirstSymbol && ch < asciiCodeSecondSymbol)
+                {
+                    sum += ch;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
