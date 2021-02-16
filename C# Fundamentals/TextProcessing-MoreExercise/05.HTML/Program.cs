c667c0fd12c6b9077e2c49b48ce7a904326df2c9
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-16 12:31:03 +0200

    Add files via upload

---
+++ C# Fundamentals/TextProcessing-MoreExercise/05.HTML/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string article = Console.ReadLine();
+            string comment = Console.ReadLine();
+            List<string> comments = new List<string>();
+            while (comment != "end of comments")
+            {
+                comments.Add(comment);
+                comment = Console.ReadLine();
+            }
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"    {title}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine($"    {article}");
+            Console.WriteLine("</article>");
+            foreach (var item in comments)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"    {item}");
+                Console.WriteLine("</div>");
+            }
+        }
+    }
+}
