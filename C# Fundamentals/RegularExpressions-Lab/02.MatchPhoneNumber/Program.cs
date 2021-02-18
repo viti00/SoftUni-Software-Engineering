b85c1b09d0ed7b7ae86b17f2ffa4ad52fa718c53
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-18 12:36:14 +0200

    Add files via upload

---
+++ C# Fundamentals/RegularExpressions-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = @"\+359([ -])2{1}\1\d{3}\1\d{4}\b";
+            var phone = Console.ReadLine();
+            var phoneMatches = Regex.Matches(phone, regex);
+
+            var matchedPhones = phoneMatches.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
