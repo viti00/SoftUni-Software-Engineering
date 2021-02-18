5b2199411b996704ab6f9a1d53527ff27c9f321f
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-18 12:36:59 +0200

    Add files via upload

---
+++ C# Fundamentals/RegularExpressions-Lab/03.MatchDates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MatchCollection dates =
+                Regex.Matches(input, @"\b(?<day>[0-3][0-9])(?:(-)|(.)|(\/))(?<month>[A-Z][a-z]{2})(\1|\2|\3)(?<year>[0-9]{4})\b");
+
+            foreach (Match date in dates)
+            {
+                Console.WriteLine($"Day: {date.Groups["day"].Value}, Month: {date.Groups["month"].Value}, Year: {date.Groups["year"].Value}");
+            }
+        }
+    }
+}
