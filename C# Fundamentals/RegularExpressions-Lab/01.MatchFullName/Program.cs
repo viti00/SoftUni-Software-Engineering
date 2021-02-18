fdc0fd5921799e609c9024be42a4e07ad5c04cd4
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-18 12:33:17 +0200

    Add files via upload

---
+++ C# Fundamentals/RegularExpressions-Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MatchFullNames1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            string input = Console.ReadLine();
+            Regex matches = new Regex(regex);
+            var matchNames = matches.Matches(input);
+            foreach (Match item in matchNames)
+            {
+                Console.Write(item.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
