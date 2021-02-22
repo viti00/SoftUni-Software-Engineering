2dbb2e38649997de641168cbc8541d919be7953a
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-22 14:20:13 +0200

    Add files via upload

---
+++ C# Fundamentals/RegularExpressions-Exercise/06.ExtractEmails/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmailExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^|\s([0-9A-Za-z]+(-*|_*|\.*)[0-9a-zA-Z]*@[a-z]+-*[a-z]*(\.[a-z]+)+\b)";
+            string input = Console.ReadLine();
+            var matched = Regex.Matches(input, pattern);
+            foreach (Match match in matched)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
