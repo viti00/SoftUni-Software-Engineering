2aca2bacc2243094a9d8b513ce2a69d5414c6315
viti00 <75797230+viti00@users.noreply.github.com>
2021-02-24 18:25:31 +0200

    Add files via upload

---
+++ C# Fundamentals/FinalExams/02.FancyBarcodes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _5.FancyBarcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"^@#+(?<barcode>[A-Z][A-Za-z0-9]{4,}[A-Z])@#+$";
+            int n = int.Parse(Console.ReadLine());
+            string curr = "";
+            string result = "";
+            for (int i = 1; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+                if (Regex.IsMatch(input, regex))
+                {
+                    var matches = Regex.Matches(input, regex);
+                    foreach (Match item in matches)
+                    {
+                        curr = item.Value;
+                    }
+                    foreach (var ch in curr)
+                    {
+                        if (char.IsDigit(ch))
+                        {
+                            result += ch.ToString();
+                        }
+                    }
+                    if (result == "")
+                    {
+                        Console.WriteLine("Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {result}");
+                    }
+                    result = "";
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+
+        }
+    }
+}
