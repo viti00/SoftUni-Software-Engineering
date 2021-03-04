b6b175846907b4c47133bd111a398269051205b2
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-04 13:41:52 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Lab/02.StackSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> numbersStack = new Stack<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbersStack.Push(numbers[i]);
+            }
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                string[] temp = command.Split();
+                if (temp[0] == "add")
+                {
+                    int firstNum = int.Parse(temp[1]);
+                    int secondNum = int.Parse(temp[2]);
+
+                    numbersStack.Push(firstNum);
+                    numbersStack.Push(secondNum);
+                }
+                else if (temp[0] == "remove")
+                {
+                    int count = int.Parse(temp[1]);
+                    int counter = count;
+                    if (count < numbersStack.Count)
+                    {
+                        while (counter != 0)
+                        {
+                            numbersStack.Pop();
+                            counter--;
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum: {numbersStack.Sum()}");
+        }
+    }
+}
---
+++ CSharp Advanced/StacksAndQueues-Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Stack<string> calculation = new Stack<string>(input.Reverse());
+
+
+            while (calculation.Count != 1)
+            {
+                int firstNum = int.Parse(calculation.Pop());
+                string sign = calculation.Pop();
+                int secondNum = int.Parse(calculation.Pop());
+                if (sign == "+")
+                {
+                    int sum = firstNum + secondNum;
+                    calculation.Push(sum.ToString());
+                }
+                else if (sign == "-")
+                {
+                    int substact = firstNum - secondNum;
+                    calculation.Push(substact.ToString());
+                }
+            }
+            Console.WriteLine(calculation.Pop());
+        }
+    }
+}
