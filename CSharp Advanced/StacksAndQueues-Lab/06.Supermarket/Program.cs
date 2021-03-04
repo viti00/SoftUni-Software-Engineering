825f9b70c7cbfde5e502e9158728a796b20769bc
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-04 13:43:06 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Lab/06.Supermarket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Queue<string> costumers = new Queue<string>();
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    while (costumers.Count != 0)
+                    {
+                        Console.WriteLine(costumers.Dequeue());
+                    }
+                }
+                else
+                {
+                    costumers.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{costumers.Count} people remaining.");
+        }
+    }
+}
