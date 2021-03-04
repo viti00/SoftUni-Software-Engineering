31cd088cc7db3dd101ead0f8910fee0e510ce51b
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-04 13:43:54 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Lab/08.TrafficJam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPassed = int.Parse(Console.ReadLine());
+            Queue<string> waiting = new Queue<string>();
+            int counter = 0;
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 1; i <= numberOfPassed; i++)
+                    {
+                        if (waiting.Count == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{waiting.Dequeue()} passed!");
+                        counter++;
+                    }
+                }
+                else
+                {
+                    waiting.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
