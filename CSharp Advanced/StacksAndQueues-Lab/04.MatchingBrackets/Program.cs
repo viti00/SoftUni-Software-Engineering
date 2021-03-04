39208f8dc064fdeee9188965d5a02dfddf2defe2
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-04 13:42:26 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> brackets = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i].ToString() == "(")
+                {
+                    brackets.Push(i);
+                }
+                else if (expression[i].ToString() == ")")
+                {
+                    brackets.Push(i);
+                    int endIndex = brackets.Pop();
+                    int startIndex = brackets.Pop();
+                    string result = expression.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
