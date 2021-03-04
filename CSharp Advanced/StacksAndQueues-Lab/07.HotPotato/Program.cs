7b2d3e1bebbe544978c9df552ba6cf6b007450d3
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-04 13:43:29 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Lab/07.HotPotato/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] players = Console.ReadLine().Split();
+            int rounds = int.Parse(Console.ReadLine());
+            Queue<string> playerNames = new Queue<string>();
+            int counter = rounds;
+            for (int i = 0; i < players.Length; i++)
+            {
+                playerNames.Enqueue(players[i]);
+            }
+
+            while (playerNames.Count != 1)
+            {
+                while (counter != 1)
+                {
+                    string player = playerNames.Dequeue();
+                    playerNames.Enqueue(player);
+                    counter--;
+                }
+                Console.WriteLine($"Removed {playerNames.Dequeue()}");
+                counter = rounds;
+            }
+            Console.WriteLine($"Last is {playerNames.Peek()}");
+        }
+    }
+}
