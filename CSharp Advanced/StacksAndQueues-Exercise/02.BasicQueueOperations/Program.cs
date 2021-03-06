6edd2a435ab9f6536a86c66373842189624e0ad9
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-06 19:22:40 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Exercise/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = nsx[0];
+            int s = nsx[1];
+            int x = nsx[2];
+
+            Queue<int> numbers = new Queue<int>();
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Enqueue(nums[i]);
+            }
+
+            for (int i = 1; i <= s; i++)
+            {
+                if (numbers.Count == 0)
+                {
+                    Console.WriteLine(numbers.Count);
+                    return;
+                }
+                numbers.Dequeue();
+            }
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(numbers.Count);
+                return;
+            }
+            if (numbers.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
+        }
+    }
+}
