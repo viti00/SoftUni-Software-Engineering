ca873a09968bda1c959b4a705a070d5995bae661
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-06 19:23:25 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Exercise/04.FastFood/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+            int[] costumersQuantity = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> orders = new Queue<int>();
+
+            for (int i = 0; i < costumersQuantity.Length; i++)
+            {
+                orders.Enqueue(costumersQuantity[i]);
+            }
+
+            Console.WriteLine(orders.Max());
+            while (quantity > 0 && orders.Count != 0)
+            {
+                if (quantity - orders.Peek() >= 0)
+                {
+                    quantity -= orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.Write("Orders left: ");
+                Console.Write(string.Join(" ", orders));
+            }
+        }
+    }
+}
