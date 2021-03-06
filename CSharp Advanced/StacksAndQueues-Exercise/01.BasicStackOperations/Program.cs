3a7f5dc1c57f6a51fae5b794e3e7c191d6ea0c45
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-06 19:15:55 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Exercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = nsx[0];
+            int s = nsx[1];
+            int x = nsx[2];
+
+            Stack<int> numbers = new Stack<int>();
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Push(nums[i]);
+            }
+
+            for (int i = 1; i <= s; i++)
+            {
+                if (numbers.Count == 0)
+                {
+                    Console.WriteLine(numbers.Count);
+                    return;
+                }
+                numbers.Pop();
+            }
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(numbers.Count);
+                return;
+            }
+            if (numbers.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
+        }
+    }
+}
