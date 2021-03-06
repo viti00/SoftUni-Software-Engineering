afc9cd4d31f23ec25aa9e63d61376e2b7909d236
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-06 19:24:03 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Exercise/06.SongsQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ");
+            Queue<string> songsQueue = new Queue<string>(songs);
+            while (songsQueue.Count != 0)
+            {
+                string command = Console.ReadLine();
+                string[] temp = command.Split();
+                string function = temp[0];
+                switch (function)
+                {
+                    case "Add":
+                        string song = "";
+                        for (int i = 1; i < temp.Length - 1; i++)
+                        {
+                            song += $"{temp[i]} ";
+                        }
+                        song += temp[temp.Length - 1];
+                        if (songsQueue.Contains(song))
+                        {
+                            Console.WriteLine($"{song} is already contained!");
+                        }
+                        else
+                        {
+                            songsQueue.Enqueue(song);
+                        }
+                        break;
+                    case "Play":
+                        songsQueue.Dequeue();
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songsQueue));
+                        break;
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
