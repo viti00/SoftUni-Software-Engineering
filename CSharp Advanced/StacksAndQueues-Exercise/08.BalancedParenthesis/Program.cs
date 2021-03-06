972416b0e14eee7cfbd17f3fc381bd2d13f54099
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-06 19:24:44 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> open = new Stack<char>();
+            string input = Console.ReadLine();
+
+            foreach (var item in input)
+            {
+                if (item == '{' || item == '[' || item == '(')
+                {
+                    open.Push(item);
+                }
+                else
+                {
+                    if (open.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                    if (item == '}' && open.Peek() == '{' || item == ']' && open.Peek() == '[' || item == ')' && open.Peek() == '(')
+                    {
+                        open.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+
+                }
+            }
+            if (open.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
