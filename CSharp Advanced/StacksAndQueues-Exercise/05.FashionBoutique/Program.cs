b4d985c6d0f8d6bb5d763566bb88a381ec492a50
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-06 19:23:44 +0200

    Add files via upload

---
+++ CSharp Advanced/StacksAndQueues-Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesValues = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+            Stack<int> clothes = new Stack<int>(clothesValues);
+
+            int rackCounter = 1;
+            int currCapacity = rackCapacity;
+            while (clothes.Count != 0)
+            {
+                if (currCapacity - clothes.Peek() >= 0)
+                {
+                    currCapacity -= clothes.Pop();
+                }
+                else
+                {
+                    currCapacity = rackCapacity;
+                    rackCounter++;
+                }
+            }
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
