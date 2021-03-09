2bf69ea31b683afb553c9f491cba255cc4ce7866
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-09 12:31:46 +0200

    Add files via upload

---
+++ CSharp Advanced/MultidimensionalArrays-Lab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += matrix[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
