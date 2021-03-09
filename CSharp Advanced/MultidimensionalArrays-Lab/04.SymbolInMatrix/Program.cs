d2e96f75e62a9f11a1742539519978781cf51708
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-09 12:32:06 +0200

    Add files via upload

---
+++ CSharp Advanced/MultidimensionalArrays-Lab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string symbols = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = symbols[col];
+                }
+            }
+
+            char neededSymbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    char currSymbol = matrix[row, col];
+
+                    if (currSymbol == neededSymbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{neededSymbol} does not occur in the matrix");
+        }
+    }
+}
