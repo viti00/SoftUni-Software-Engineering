252ded9d16a1d9e6cdcb7266ad5e967e2e20a748
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-11 12:42:46 +0200

    Add files via upload

---
+++ CSharp Advanced/MultidimensionalArrays-Exercise/07.KnightGame/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace _07.KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] board = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] rowData = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    board[row, col] = rowData[col];
+                }
+            }
+
+            int removedKnightCounts = 0;
+
+            while (true)
+            {
+                int maxAttackedKnightCount = 0;
+                int knightRow = -1;
+                int knightCol = -1;
+
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        char symbol = board[row, col];
+                        if (symbol != 'K')
+                        {
+                            continue;
+                        }
+                        int count = GetCountAttackedKnight(board, row, col);
+                        if (count > maxAttackedKnightCount)
+                        {
+                            maxAttackedKnightCount = count;
+                            knightRow = row;
+                            knightCol = col;
+                        }
+                    }
+                }
+                if (maxAttackedKnightCount == 0)
+                {
+                    break;
+                }
+                board[knightRow, knightCol] = 'О';
+                removedKnightCounts++;
+            }
+            Console.WriteLine(removedKnightCounts);
+        }
+        private static int GetCountAttackedKnight(char[,] board, int row, int col)
+        {
+            int count = 0;
+
+            if (ContainstKnight(board, row - 2, col - 1))
+            {
+                count++;
+            }
+            if (ContainstKnight(board, row - 2, col + 1))
+            {
+                count++;
+            }
+            if (ContainstKnight(board, row - 1, col + 2))
+            {
+                count++;
+            }
+            if (ContainstKnight(board, row - 1, col - 2))
+            {
+                count++;
+            }
+            if (ContainstKnight(board, row + 1, col + 2))
+            {
+                count++;
+            }
+            if (ContainstKnight(board, row + 1, col - 2))
+            {
+                count++;
+            }
+            if (ContainstKnight(board, row + 2, col - 1))
+            {
+                count++;
+            }
+            if (ContainstKnight(board, row + 2, col + 1))
+            {
+                count++;
+            }
+            return count;
+        }
+        private static bool ContainstKnight(char[,] board, int row, int col)
+        {
+            if (!IsValidCell(row, col, board.GetLength(0)))
+            {
+                return false;
+            }
+            return board[row, col] == 'K';
+        }
+        private static bool IsValidCell(int row, int col, int lengh)
+        {
+            return row >= 0 && row < lengh && col >= 0 && col < lengh;
+        }
+    }
+}
---
+++ CSharp Advanced/MultidimensionalArrays-Exercise/08.Bombs/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = elements[col];
+                }
+            }
+
+            string[] cordinates = Console.ReadLine().Split();
+            for (int i = 0; i < cordinates.Length; i++)
+            {
+                string[] temp = cordinates[i].Split(",");
+
+                int row1 = int.Parse(temp[0]);
+                int col1 = int.Parse(temp[1]);
+
+                if (matrix[row1, col1] > 0)
+                {
+                    int currPower = matrix[row1, col1];
+                    matrix[row1, col1] = 0;
+                    if (row1 - 1 >= 0 && col1 - 1 >= 0)
+                    {
+                        if (matrix[row1 - 1, col1 - 1] > 0)
+                        {
+                            matrix[row1 - 1, col1 - 1] -= currPower;
+                        }
+
+                    }
+                    if (row1 - 1 >= 0 && col1 >= 0)
+                    {
+                        if (matrix[row1 - 1, col1] > 0)
+                        {
+                            matrix[row1 - 1, col1] -= currPower;
+                        }
+                    }
+                    if (row1 - 1 >= 0 && col1 + 1 < n)
+                    {
+                        if (matrix[row1 - 1, col1 + 1] > 0)
+                        {
+                            matrix[row1 - 1, col1 + 1] -= currPower;
+                        }
+
+                    }
+                    if (row1 >= 0 && col1 + 1 < n)
+                    {
+                        if (matrix[row1, col1 + 1] > 0)
+                        {
+                            matrix[row1, col1 + 1] -= currPower;
+                        }
+
+                    }
+                    if (row1 + 1 < n && col1 + 1 < n)
+                    {
+                        if (matrix[row1 + 1, col1 + 1] > 0)
+                        {
+                            matrix[row1 + 1, col1 + 1] -= currPower;
+                        }
+                    }
+                    if (row1 + 1 < n && col1 >= 0)
+                    {
+                        if (matrix[row1 + 1, col1] > 0)
+                        {
+                            matrix[row1 + 1, col1] -= currPower;
+                        }
+
+                    }
+                    if (row1 + 1 < n && col1 - 1 >= 0)
+                    {
+                        if (matrix[row1 + 1, col1 - 1] > 0)
+                        {
+                            matrix[row1 + 1, col1 - 1] -= currPower;
+                        }
+
+                    }
+                    if (row1 >= 0 && col1 - 1 >= 0)
+                    {
+                        if (matrix[row1, col1 - 1] > 0)
+                        {
+                            matrix[row1, col1 - 1] -= currPower;
+                        }
+
+                    }
+                }
+            }
+            int aliveSum = 0;
+            int aliveCounter = 0;
+
+            foreach (var item in matrix)
+            {
+                if (item > 0)
+                {
+                    aliveSum += item;
+                    aliveCounter++;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {aliveCounter}");
+            Console.WriteLine($"Sum: {aliveSum}");
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
