178536b9a99f6155ad43d064a98aca4d9a2908f5
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-17 17:27:20 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Lab/06.ParkingLot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            HashSet<string> cars = new HashSet<string>();
+
+            while (command != "END")
+            {
+                string[] temp = command.Split(", ");
+
+                string direction = temp[0];
+                string carNumber = temp[1];
+
+                if (direction == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else if (direction == "OUT")
+                {
+                    cars.Remove(carNumber);
+                }
+                command = Console.ReadLine();
+            }
+
+            if (cars.Count > 0)
+            {
+                foreach (var carNumber in cars)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
