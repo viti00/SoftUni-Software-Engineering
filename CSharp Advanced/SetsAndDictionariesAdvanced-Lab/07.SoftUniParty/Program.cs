d1e703b2e6b146afdb971b34a9d010b607431435
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-17 17:27:41 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Lab/07.SoftUniParty/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guestsList = new HashSet<string>();
+
+            string command = Console.ReadLine();
+            while (command != "PARTY")
+            {
+                guestsList.Add(command);
+                command = Console.ReadLine();
+            }
+
+            string afterPartyCommand = Console.ReadLine();
+            while (afterPartyCommand != "END")
+            {
+                guestsList.Remove(afterPartyCommand);
+                afterPartyCommand = Console.ReadLine();
+            }
+
+            Console.WriteLine(guestsList.Count);
+
+            foreach (var guest in guestsList)
+            {
+                char[] ch = guest.ToCharArray();
+                if (char.IsDigit(ch[0]))
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+            foreach (var guest in guestsList)
+            {
+                char[] ch = guest.ToCharArray();
+                if (char.IsLetter(ch[0]))
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+        }
+    }
+}
