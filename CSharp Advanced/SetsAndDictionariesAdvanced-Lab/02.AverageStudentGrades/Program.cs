1304dc7d4558f441ca95b0148b1cb74127c83051
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-17 17:24:20 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Lab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<decimal>> grades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string name = data[0];
+                decimal grade = decimal.Parse(data[1]);
+
+                if (!grades.ContainsKey(name))
+                {
+                    grades.Add(name, new List<decimal>());
+                }
+                grades[name].Add(grade);
+            }
+
+            foreach (var student in grades)
+            {
+                Console.Write($"{student.Key} -> ");
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+                Console.WriteLine($"(avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
