6c6f422407152cc0d06a6efa01b667717eae25b2
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-17 17:26:29 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Lab/04.CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> information = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] datas = Console.ReadLine().Split();
+
+                string continent = datas[0];
+                string country = datas[1];
+                string city = datas[2];
+
+                if (!information.ContainsKey(continent))
+                {
+                    information.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!information[continent].ContainsKey(country))
+                {
+                    information[continent].Add(country, new List<string>());
+                }
+                information[continent][country].Add(city);
+            }
+
+            foreach (var continent in information)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var country in continent.Value)
+                {
+                    Console.Write($"{country.Key} -> ");
+                    Console.Write(string.Join(", ", country.Value));
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
