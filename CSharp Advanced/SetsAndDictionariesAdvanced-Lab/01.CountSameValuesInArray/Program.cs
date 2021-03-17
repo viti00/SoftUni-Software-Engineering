7dcb73ae676c899b45bd02e5470dbd364e2bb707
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-17 17:17:56 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            Dictionary<double, int> times = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!times.ContainsKey(numbers[i]))
+                {
+                    times.Add(numbers[i], 0);
+                }
+                times[numbers[i]] += 1;
+            }
+
+            foreach (var item in times)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
