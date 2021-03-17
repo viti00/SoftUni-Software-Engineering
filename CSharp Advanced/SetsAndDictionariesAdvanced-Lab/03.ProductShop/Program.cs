3f950b229e3de6855b0c692660d34e7c922220ba
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-17 17:25:52 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Lab/03.ProductShop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> products = new Dictionary<string, Dictionary<string, double>>();
+
+            string command = Console.ReadLine();
+            while (command != "Revision")
+            {
+                string[] temp = command.Split(", ");
+
+                string market = temp[0];
+                string product = temp[1];
+                double price = double.Parse(temp[2]);
+
+                if (!products.ContainsKey(market))
+                {
+                    products.Add(market, new Dictionary<string, double>());
+                }
+
+                if (!products[market].ContainsKey(product))
+                {
+                    products[market].Add(product, price);
+                }
+                command = Console.ReadLine();
+            }
+
+            foreach (var market in products.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{market.Key}->");
+                foreach (var product in market.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
