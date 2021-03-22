b9c83ba0c96ce233d62f6e90abaf2514b13759b0
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-22 13:41:00 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Exercise/05.CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> charTimes = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+                if (!charTimes.ContainsKey(symbol))
+                {
+                    charTimes.Add(symbol, 0);
+                }
+                charTimes[symbol]++;
+            }
+
+            foreach (var ch in charTimes.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+        }
+    }
+}
