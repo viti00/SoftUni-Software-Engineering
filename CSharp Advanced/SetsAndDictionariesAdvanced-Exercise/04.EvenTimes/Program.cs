0c85114292a13d906c1084a7dd5d1186e0e455b8
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-22 13:40:41 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> evenTimes = new Dictionary<int, int>();
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!evenTimes.ContainsKey(number))
+                {
+                    evenTimes.Add(number, 0);
+                }
+                evenTimes[number]++;
+            }
+
+            foreach (var number in evenTimes)
+            {
+                if (number.Value % 2 == 0)
+                {
+                    Console.WriteLine(number.Key);
+                }
+            }
+        }
+    }
+}
