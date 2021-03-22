afd83a25be1d3c09a3c45785afbbd7e3437df794
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-22 13:40:16 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    chemicalElements.Add(elements[j]);
+                }
+            }
+
+            foreach (var element in chemicalElements)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
