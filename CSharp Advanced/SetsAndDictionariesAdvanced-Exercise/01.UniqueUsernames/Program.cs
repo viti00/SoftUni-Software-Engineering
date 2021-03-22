3ab64e6c19f5d98f761f5622b08a09cd2d72702f
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-22 13:34:05 +0200

    Add files via upload

---
+++ CSharp Advanced/SetsAndDictionariesAdvanced-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
