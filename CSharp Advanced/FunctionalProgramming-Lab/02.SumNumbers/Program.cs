32a4de512e2bc14b7c4952ab641e4286657a45c9
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-29 12:54:02 +0300

    Add files via upload

---
+++ CSharp Advanced/FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSumAndCount(
+                int.Parse,
+                a => a.Length,
+                number =>
+                {
+                    int sum = 0;
+                    sum = number.Sum();
+                    return sum;
+                });
+
+
+        }
+        static void PrintSumAndCount(Func<string, int> parser,
+            Func<int[], int> getCount,
+            Func<int[], int> getSum)
+        {
+
+
+            int[] numbers = Console.ReadLine()
+                 .Split(", ")
+                 .Select(parser)
+                 .ToArray();
+
+
+            Console.WriteLine(getCount(numbers));
+            Console.WriteLine(getSum(numbers));
+
+        }
+    }
+}
