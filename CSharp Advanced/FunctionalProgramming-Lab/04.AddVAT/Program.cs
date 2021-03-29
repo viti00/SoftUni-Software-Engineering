ffd611e98843d04ebc237e7564d319f506e5bbe7
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-29 12:55:20 +0300

    Add files via upload

---
+++ CSharp Advanced/FunctionalProgramming-Lab/04.AddVAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(decimal.Parse)
+                .Select(x => x + (x * 0.2m))
+                .Select(x => x = Math.Round(x, 2))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, numbers));
+        }
+    }
+}
