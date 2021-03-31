82ad29a2a6aaf12cca7b0f69dd1d9222b65b9c63
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-31 16:42:59 +0300

    Add files via upload

---
+++ CSharp Advanced/FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                                        .Split()
+                                        .ToList();
+
+            Func<List<string>, List<string>> getNames = names =>
+            {
+                List<string> correctNames = new List<string>();
+                foreach (var name in names)
+                {
+                    if (name.Length <= n)
+                    {
+                        correctNames.Add(name);
+                    }
+                }
+                return correctNames;
+            };
+
+            List<string> corretNames = getNames(names);
+
+            Console.WriteLine(string.Join(Environment.NewLine, corretNames));
+        }
+    }
+}
