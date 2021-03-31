aa01d9ae787b34a130dbbd6cfc9aed1247d9775a
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-31 16:44:50 +0300

    Add files via upload

---
+++ CSharp Advanced/FunctionalProgramming-Exercise/11.TriFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isValidWord = (str, num) => str.ToCharArray()
+                            .Select(ch => (int)ch).Sum() >= num;
+
+            Func<string[], int, Func<string, int, bool>, string> firstValidName = (arr, num, func) => arr
+                .FirstOrDefault(str => func(str, num));
+
+            var result = firstValidName(names, num, isValidWord);
+            Console.WriteLine(result);
+        }
+    }
+}
