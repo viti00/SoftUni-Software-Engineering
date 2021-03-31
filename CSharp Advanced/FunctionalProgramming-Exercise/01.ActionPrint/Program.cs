a9067b969cee4d640fd77cf5298a46c95739b739
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-31 16:35:07 +0300

    Add files via upload

---
+++ CSharp Advanced/FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split();
+            Action<string> printStrings = (x) => Console.WriteLine(x);
+            strings.ToList().ForEach(x => printStrings(x));
+
+        }
+    }
+}
