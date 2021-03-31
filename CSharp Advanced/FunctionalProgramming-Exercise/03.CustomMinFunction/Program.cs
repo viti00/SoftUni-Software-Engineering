e39d1e7f4fe13625bd306ca5e894761c9bbf66d5
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-31 16:41:26 +0300

    Add files via upload

---
+++ CSharp Advanced/FunctionalProgramming-Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> printer = GetMinValue;
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(printer(numbers));
+
+        }
+        static int GetMinValue(int[] numbers)
+        {
+            int min = int.MaxValue;
+            if (numbers.Length > 0)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int curNum = numbers[i];
+                    if (curNum < min)
+                    {
+                        min = curNum;
+                    }
+                }
+            }
+            return min;
+        }
+    }
+}
