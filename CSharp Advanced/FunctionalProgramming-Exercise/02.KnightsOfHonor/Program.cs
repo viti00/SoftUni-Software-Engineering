7c345b921afa20c5a6a5ba55097cd7a635a3cc76
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-31 16:40:51 +0300

    Add files via upload

---
+++ CSharp Advanced/FunctionalProgramming-Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split();
+
+            Action<string> printer = (x) => Console.WriteLine($"Sir {x}");
+            strings.ToList().ForEach(x => printer(x));
+        }
+    }
+}
