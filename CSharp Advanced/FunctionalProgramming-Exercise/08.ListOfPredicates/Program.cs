3c9219473d6999f9b9e46effb647447d3aa69fa9
viti00 <75797230+viti00@users.noreply.github.com>
2021-03-31 16:43:29 +0300

    Add files via upload

---
+++ CSharp Advanced/FunctionalProgramming-Exercise/08.ListOfPredicates/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int endIndex = int.Parse(Console.ReadLine());
+
+            int[] dividers = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            Func<int, List<int>> getSequence = endIndex =>
+            {
+                List<int> sequence = new List<int>();
+                for (int i = 1; i <= endIndex; i++)
+                {
+                    sequence.Add(i);
+                }
+                return sequence;
+            };
+
+            List<int> sequence = getSequence(endIndex);
+
+            Predicate<int> predicate = num =>
+            {
+                int counter = 0;
+                for (int i = 0; i < dividers.Length; i++)
+                {
+                    if (num % dividers[i] == 0)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == dividers.Length)
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            Func<List<int>, Predicate<int>, List<int>> getCorrectNumbers = (sequence, predicate) =>
+            {
+                List<int> correctNumbes = new List<int>();
+
+                for (int i = 0; i < sequence.Count; i++)
+                {
+                    if (predicate(sequence[i]) == true)
+                    {
+                        correctNumbes.Add(sequence[i]);
+                    }
+                }
+
+                return correctNumbes;
+            };
+
+            List<int> correctNumbers = getCorrectNumbers(sequence, predicate);
+
+            Console.WriteLine(string.Join(" ", correctNumbers));
+        }
+    }
+}
