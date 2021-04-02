52c2e396bdb545ae22fdeaf0d7503ab803d7b595
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-02 11:16:42 +0300

    Add files via upload

---
+++ CSharp Advanced/DefiningClasses-Lab/03.CarConstructors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            firstCar.Drive(200);
+            Console.WriteLine(firstCar.WhoAmI());
+
+            Car secondCar = new Car(make, model, year);
+            secondCar.Drive(200);
+            Console.WriteLine(secondCar.WhoAmI());
+
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
+            thirdCar.Drive(200);
+            Console.WriteLine(thirdCar.WhoAmI());
+        }
+    }
+}
