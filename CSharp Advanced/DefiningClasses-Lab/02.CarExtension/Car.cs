f91565753ce3aa0f4bd096bb88fb7af4fd58abb6
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-02 11:16:11 +0300

    Add files via upload

---
+++ CSharp Advanced/DefiningClasses-Lab/02.CarExtension/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * FuelConsumption;
+            if (FuelQuantity - consumption >= 0)
+            {
+                FuelQuantity -= consumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
