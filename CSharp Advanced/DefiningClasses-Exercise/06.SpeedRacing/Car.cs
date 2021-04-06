1f23558bb2ae885699ef80d57e42fe9796a38a45
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-06 16:19:14 +0300

    Add files via upload

---
+++ CSharp Advanced/DefiningClasses-Exercise/06.SpeedRacing/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace _06.SpeedRacing
+{
+    public class Car
+    {
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public void Drive(string model, double distance, Dictionary<string, List<double>> cars)
+        {
+            if(cars[model][0] >= cars[model][1] * distance)
+            {
+                cars[model][0] -= (cars[model][1] * distance);
+                cars[model][2] += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/DefiningClasses-Exercise/06.SpeedRacing/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SpeedRacing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> listCars = new Dictionary<string, List<double>>();
+
+            Car car = new Car();
+            for (int i = 1; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] splitted = input.Split();
+
+                car.Model = splitted[0];
+                car.FuelAmount = int.Parse(splitted[1]);
+                car.FuelConsumptionPerKilometer = double.Parse(splitted[2]);
+                car.TravelledDistance = 0;
+
+                listCars.Add(car.Model, new List<double> { car.FuelAmount, car.FuelConsumptionPerKilometer, car.TravelledDistance });
+            }
+            string command = Console.ReadLine();
+            while(command != "End")
+            {
+                string[] splitted = command.Split();
+
+                string model = splitted[1];
+                double distance = double.Parse(splitted[2]);
+
+                car.Drive(model, distance, listCars);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var cars in listCars)
+            {
+                Console.WriteLine($"{cars.Key} {cars.Value[0]:F2} {cars.Value[2]}");
+            }
+        }
+    }
+}
