49cdc13dd42ea064f8134941e0efe0cb9f83feb8
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-06 16:16:30 +0300

    Add files via upload

---
+++ CSharp Advanced/DefiningClasses-Exercise/02.CreatingConstructors/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person person = new Person();
+            Console.WriteLine($"{person.Name} is {person.Age} years old");
+
+            
+            Person personWithAge = new Person(age);
+            personWithAge.Age = age;
+            Console.WriteLine($"{personWithAge.Name} is {personWithAge.Age} years old");
+
+            Person personNameAndAge = new Person(name, age);
+            personNameAndAge.Name = name;
+            personNameAndAge.Age = age;
+            Console.WriteLine($"{personNameAndAge.Name} is {personNameAndAge.Age} years old");
+
+        }
+    }
+}
