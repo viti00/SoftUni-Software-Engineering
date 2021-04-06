6c1af9f90644ebd52477f91e02099f4025ad7451
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-06 16:18:44 +0300

    Add files via upload

---
+++ CSharp Advanced/DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.DateModifier
+{
+    public class DateModifier
+    {
+        public DateModifier(string firstDate, string secondDate)
+        {
+            FirstDate = firstDate;
+            SecondDate = secondDate;
+        }
+
+        public string FirstDate { get; set; }
+        public string SecondDate { get; set; }
+
+
+        public double GetDifference (string firstDate, string secondDate)
+        {
+            double daysDifference = 0;
+            var parseFirst = DateTime.Parse(firstDate);
+            var parseSecond = DateTime.Parse(secondDate);
+            daysDifference = (parseFirst - parseSecond).TotalDays;
+            daysDifference = Math.Abs(daysDifference);
+            return daysDifference;
+        }
+    }
+}
---
+++ CSharp Advanced/DefiningClasses-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05.DateModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            DateModifier dateModifire = new DateModifier(firstDate, secondDate);
+
+            Console.WriteLine(dateModifire.GetDifference(dateModifire.FirstDate, dateModifire.SecondDate));
+        }
+    }
+}
