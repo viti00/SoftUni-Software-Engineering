24b7587e313fe283803b58b90feb13fda2c3978a
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-06 16:15:59 +0300

    Add files via upload

---
+++ CSharp Advanced/DefiningClasses-Exercise/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = Console.ReadLine();
+            person.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{person.Name} is {person.Age} years old");
+        }
+    }
+}
