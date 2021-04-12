220e58b40837ef0876ab7dd6d61d51c7ab3ea0e1
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-12 13:18:55 +0300

    Add files via upload

---
+++ CSharp Advanced/Generics-Lab/01.Box/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        Stack<T> elements;
+
+        public Box()
+        {
+            elements = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            elements.Push(element);
+        }
+
+        public T Remove()
+        {
+            T elementToRemove = elements.Pop();
+            return elementToRemove;
+        }
+
+        public int Count { get { return elements.Count; } }
+    }
+}
