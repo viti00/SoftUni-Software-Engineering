e2d53c6ce80a62b20f4e1799eb86af6a530837c9
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-16 14:04:04 +0300

    Add files via upload

---
+++ CSharp Advanced/Generics-Exercise/03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Box<T>
+    {
+        List<T> elements;
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+        }
+
+        public void Swap (int firstIndex, int secondIndex)
+        {
+            if (firstIndex >= 0 && firstIndex < elements.Count &&
+                secondIndex >= 0 && secondIndex < elements.Count)
+            {
+                T firstElement = elements[firstIndex];
+                T secondElement = elements[secondIndex];
+
+                elements[firstIndex] = secondElement;
+                elements[secondIndex] = firstElement;
+            }
+        }
+
+        public void Foreach()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                Console.WriteLine($"{elements[i].GetType()}: {elements[i]}");
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/Generics-Exercise/03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> listElements = new Box<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                var element = Console.ReadLine();
+
+                listElements.Add(element);
+            }
+
+            int[] swapCommand = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int firstIndex = swapCommand[0];
+            int secondIndex = swapCommand[1];
+
+            listElements.Swap(firstIndex, secondIndex);
+
+            listElements.Foreach();
+        }
+    }
+}
