2ed7e639697f35cf9714d1513d059076a6bd778e
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-16 14:02:25 +0300

    Add files via upload

---
+++ CSharp Advanced/Generics-Exercise/01.GenericBoxОfString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.GenericBoxОfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> listElements = new Box<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                var element = Console.ReadLine();
+
+                listElements.Add(element);
+            }
+            listElements.Foreach();
+
+        }
+    }
+}
