f1b21e8ed42a6c17acfeddce3ef29f7862134453
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-16 14:03:00 +0300

    Add files via upload

---
+++ CSharp Advanced/Generics-Exercise/02.GenericBoxОfInteger/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.GenericBoxОfInteger
+{
+    public class Box<T>
+    {
+        List<T> elements;
+
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            elements.Add(item);
+        }
+
+        public void Foreach()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                Console.WriteLine($"{elements[i].GetType()}: {elements[i]}");
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/Generics-Exercise/02.GenericBoxОfInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.GenericBoxОfInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> listElements = new Box<int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                var element = int.Parse(Console.ReadLine());
+
+                listElements.Add(element);
+            }
+            listElements.Foreach();
+        }
+    }
+}
