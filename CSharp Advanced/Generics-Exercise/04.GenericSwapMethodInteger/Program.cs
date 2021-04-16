cc981c08e20098c2b5c120c13fd445d0fa84aa04
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-16 14:04:59 +0300

    Add files via upload

---
+++ CSharp Advanced/Generics-Exercise/04.GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> listElements = new Box<int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                var element = int.Parse(Console.ReadLine());
+                listElements.Add(element);
+            }
+
+            int[] swapCommand = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int firstIndex = swapCommand[0];
+            int secondIndex = swapCommand[1];
+
+            listElements.Swap(firstIndex, secondIndex);
+
+            listElements.Foreach();
+        }
+    }
+}
