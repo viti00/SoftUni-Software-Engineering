7159b12fd69e99cb71ce967c47d839179e7e6d65
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-16 14:07:15 +0300

    Add files via upload

---
+++ CSharp Advanced/Generics-Exercise/08.Threeuple/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace _08.Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Threeuple<string, string, string> threeuple1 = new Threeuple<string, string, string>();
+            string firstInput = Console.ReadLine();
+
+            string[] splitted = firstInput.Split();
+
+            string fullName = $"{splitted[0]} {splitted[1]}";
+            string adres = splitted[2];
+            string town = " ";
+            if (splitted.Length > 4)
+            {
+                town = $"{splitted[3]} {splitted[4]}";
+            }
+            else
+            {
+                town = splitted[3];
+            }
+            
+
+            threeuple1.Item1 = fullName;
+            threeuple1.Item2 = adres;
+            threeuple1.Item3 = town;
+
+            Threeuple<string, int, bool> threeuple2 = new Threeuple<string, int, bool>();
+
+            string secondInput = Console.ReadLine();
+            splitted = secondInput.Split();
+
+            string name = splitted[0];
+            int year = int.Parse(splitted[1]);
+            bool IsDrunk = false;           
+
+            threeuple2.Item1 = name;
+            threeuple2.Item2 = year;
+            threeuple2.Item3 = GetRuslt(splitted, IsDrunk);
+
+            Threeuple<string, double, string> threeuple3 = new Threeuple<string, double, string>();
+
+            string thirdInput = Console.ReadLine();
+            splitted = thirdInput.Split();
+
+            string firstName = splitted[0];
+            double balance = double.Parse(splitted[1]);
+            string bankName = splitted[2];
+
+            threeuple3.Item1 = firstName;
+            threeuple3.Item2 = balance;
+            threeuple3.Item3 = bankName;
+
+            Console.WriteLine($"{threeuple1.Item1} -> {threeuple1.Item2} -> {threeuple1.Item3}");
+            Console.WriteLine($"{threeuple2.Item1} -> {threeuple2.Item2} -> {threeuple2.Item3}");
+            Console.WriteLine($"{threeuple3.Item1} -> {threeuple3.Item2} -> {threeuple3.Item3}");
+        }
+        static bool GetRuslt(string[] splitted, bool IsDrunk)
+        {
+            if (splitted[2] == "drunk")
+            {
+                IsDrunk = true;
+            }
+
+            return IsDrunk;
+        }
+    }
+}
---
+++ CSharp Advanced/Generics-Exercise/08.Threeuple/Threeuple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+    }
+}
