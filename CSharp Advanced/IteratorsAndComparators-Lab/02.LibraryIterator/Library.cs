01e149f41440d815758090f7b32683350c9a1fdd
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-18 20:07:26 +0300

    Add files via upload

---
+++ CSharp Advanced/IteratorsAndComparators-Lab/02.LibraryIterator/Library.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerator<Book>
+    {
+        public Library(params Book[] books)
+        {
+            Books = books.ToList();
+        }
+
+        public List<Book> Books { get; set; }
+
+        private int index = -1;
+
+        public Book Current => Books[index];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            if (Books.Count <= index)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+}
