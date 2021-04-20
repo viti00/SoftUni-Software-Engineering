5b8b3a985f8d3c985296dc1ba98ffebbc3e00edf
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-20 14:10:15 +0300

    Add files via upload

---
+++ CSharp Advanced/IteratorsАndComparators-Exercise/02.Collection/ListIterator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Collection
+{
+    public class ListIterator<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        private int currentIndex;
+
+        public ListIterator(IEnumerable<T> inputElements)
+        {
+            elements = new List<T>(inputElements);
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (currentIndex + 1 >= elements.Count)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void Print()
+        {
+            if (elements.Count > 0 && currentIndex >= 0 && currentIndex < elements.Count)
+            {
+                Console.WriteLine(elements[currentIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+        public string PrintAll()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < elements.Count; i++)
+            {
+                sb.Append($"{elements[i]} ");
+            }
+            return sb.ToString().Trim();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CSharp Advanced/IteratorsАndComparators-Exercise/02.Collection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Collection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListIterator<string> listElements = null;
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] splitted = command.Split();
+                switch (splitted[0])
+                {
+                    case "Create":
+                        listElements = new ListIterator<string>(splitted.Skip(1));
+                        break;
+                    case "Move":
+                        Console.WriteLine(listElements.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listElements.HasNext());
+                        break;
+                    case "Print":
+                        listElements.Print();
+                        break;
+                    case "PrintAll":
+                        Console.WriteLine(listElements.PrintAll());
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
