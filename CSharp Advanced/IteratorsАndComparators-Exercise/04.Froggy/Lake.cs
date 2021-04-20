99a0d84e8b9647a933dbefcd99f4c0e639a10fc5
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-20 14:11:17 +0300

    Add files via upload

---
+++ CSharp Advanced/IteratorsАndComparators-Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake<T> : IEnumerable<int>
+    {
+        List<int> stones;
+        public Lake(IEnumerable<int> lakeStones)
+        {
+            stones = new List<int>(lakeStones);
+        }
+
+        public Lake()
+        {
+            stones = new List<int>();
+        }
+
+        public void JumpEven(List<int> even)
+        {
+            for (int i = 0; i < even.Count; i++)
+            {
+                stones.Add(even[i]);
+            }
+        }
+
+        public void JumpOdd(List<int> odd)
+        {
+            for (int i = odd.Count - 1; i >= 0; i--)
+            {
+                stones.Add(odd[i]);
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Count; i++)
+            {
+                yield return stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", stones);
+        }
+    }
+}
---
+++ CSharp Advanced/IteratorsАndComparators-Exercise/04.Froggy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Lake<int> lakeStones = new Lake<int>();
+            int[] stones = Console.ReadLine()
+                                  .Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            List<int> evenStone = new List<int>();
+            List<int> oddStone = new List<int>();
+
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenStone.Add(stones[i]);
+                }
+                else
+                {
+                    oddStone.Add(stones[i]);
+                }
+            }
+
+            lakeStones.JumpEven(evenStone);
+            lakeStones.JumpOdd(oddStone);
+
+            Console.WriteLine(lakeStones);
+        }
+    }
+}
