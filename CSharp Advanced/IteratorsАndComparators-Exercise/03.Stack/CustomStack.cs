a48800639ddc12c8c0ae1461aa991935f8f1812f
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-20 14:10:43 +0300

    Add files via upload

---
+++ CSharp Advanced/IteratorsАndComparators-Exercise/03.Stack/CustomStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        List<T> elements;
+
+        public CustomStack(IEnumerable<T> items)
+        {
+            elements = new List<T>(items);
+        }
+
+        public CustomStack()
+        {
+            elements = new List<T>();
+        }
+
+        public void Push(T element)
+        {
+            elements.Add(element);
+        }
+
+        public void Pop()
+        {
+            if (elements.Count == 0)
+            {
+                Console.WriteLine("No elements");
+            }
+            else
+            {
+                elements.Remove(elements[elements.Count - 1]);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharp Advanced/IteratorsАndComparators-Exercise/03.Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace _03.Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> elements = new CustomStack<string>();
+            string input = Console.ReadLine();
+            string[] split = input.Split(new char[] { ' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+            elements = new CustomStack<string>((split.Skip(1)));
+
+            string command = Console.ReadLine();
+            while(command != "END")
+            {
+                string[] splitted = command.Split();
+
+                switch (splitted[0])
+                {
+                    case "Push":
+                        string element = splitted[1];
+                        elements.Push(element);
+                        break;
+                    case "Pop":
+                        elements.Pop();
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in elements)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in elements)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
