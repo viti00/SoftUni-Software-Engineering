2c6d94ad8115470084442b6b94b65eedd56d1ce7
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-20 14:09:42 +0300

    Add files via upload

---
+++ CSharp Advanced/IteratorsАndComparators-Exercise/01.ListyIterator/ListIterator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListIterator<T>
+    {
+        private List<T> elements;
+        private int currentIndex;
+
+        public ListIterator(IEnumerable<T> inputElements)
+        {
+            elements = new List<T>(inputElements);
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                currentIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (currentIndex + 1 >= elements.Count)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void Print()
+        {
+            if (elements.Count > 0 && currentIndex >= 0 && currentIndex < elements.Count)
+            {
+                Console.WriteLine(elements[currentIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/IteratorsАndComparators-Exercise/01.ListyIterator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListIterator<string> listElements = null;
+            string command = Console.ReadLine();
+            while(command != "END")
+            {
+                string[] splitted = command.Split();
+                switch (splitted[0])
+                {
+                    case "Create":
+                        listElements = new ListIterator<string>(splitted.Skip(1));
+                        break;
+                    case "Move":
+                        Console.WriteLine(listElements.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listElements.HasNext());
+                        break;
+                    case "Print":
+                        listElements.Print();
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            
+        }
+    }
+}
