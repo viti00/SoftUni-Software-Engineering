3e1dadc982c42f109f0c933b308e0faf4f8b0034
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-22 16:04:33 +0300

    Add files via upload

---
+++ CSharp Advanced/AdvancedExams/01.Scheduling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tasks = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[] threads = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int value = int.Parse(Console.ReadLine());
+
+            Stack<int> tasksStack = new Stack<int>(tasks);
+            Queue<int> queueThreads = new Queue<int>(threads);
+            while (true)
+            {
+
+                if (tasksStack.Peek() == value)
+                {
+                    Console.WriteLine($"Thread with value {queueThreads.Peek()} killed task {tasksStack.Peek()}");
+                    Console.WriteLine(string.Join(" ", queueThreads));
+                    return;
+                }
+                if (queueThreads.Peek() >= tasksStack.Peek() && tasksStack.Peek() != value)
+                {
+                    queueThreads.Dequeue();
+                    tasksStack.Pop();
+                }
+                else
+                {
+                    queueThreads.Dequeue();
+                }
+            }
+        }
+    }
+}
