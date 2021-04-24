51b5df5d8e7bfc0ca3d91addd31595f3698cd935
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-24 19:17:59 +0300

    Add files via upload

---
+++ CSharp Advanced/AdvancedExams/VetClinic/Clinic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> pets;
+
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            pets = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+        public int Count { get { return pets.Count; } }
+
+        public void Add(Pet pet)
+        {
+            if (pets.Count < Capacity)
+            {
+                pets.Add(pet);
+            }
+        }
+
+        public bool Remove (string name)
+        {
+            Pet pet = pets.FirstOrDefault(p => p.Name == name);
+            if (pet == null)
+            {
+                return false;
+            }
+            pets.Remove(pet);
+            return true;
+        }
+
+        public Pet GetPet (string name, string owner)
+        {
+            Pet pet = pets.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            return pets.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var pet in pets)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString();
+        }
+    }
+}
