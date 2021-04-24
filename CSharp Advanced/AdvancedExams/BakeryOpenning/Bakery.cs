12e2fbbb890bd4d26c9c787e86213b4d8b100d14
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-24 19:15:47 +0300

    Add files via upload

---
+++ CSharp Advanced/AdvancedExams/BakeryOpenning/Bakery.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        public Bakery(string name, int capacity, List<Employee> employees)
+        {
+            Name = name;
+            Capacity = capacity;
+            Employees = employees;
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Employee> Employees { get; set; }
+        private int counter = 0;
+        
+
+        public List<Employee> Add(Employee employee)
+        {
+            if (counter < Capacity)
+            {
+                Employees.Add(employee);
+                counter++;
+            }
+            return Employees;
+        }
+
+        public bool Remove (string name)
+        {
+            foreach (var item in Employees)
+            {
+                if (item.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Employee GetOldestEmpoloyee()
+        {
+            int oldest = int.MinValue;
+            string nameOldestPeople = "";
+            foreach (var item in Employees)
+            {
+                if (item.Age > oldest)
+                {
+                    oldest = item.Age;
+                    nameOldestPeople = item.Name;
+                }
+            }
+            foreach (var item in Employees)
+            {
+                if(item.Name == nameOldestPeople)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        public Employee GetEmployee (string name)
+        {
+            foreach (var item in Employees)
+            {
+                if (item.Name == name)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        public int Count()
+        {
+            return Employees.Count;
+        }
+        public void Report()
+        {
+            Console.WriteLine($"Employees working at Bakery {Name}:");
+            foreach (var item in Employees)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/AdvancedExams/BakeryOpenning/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BakeryOpenning
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Bakery bakery = new Bakery("Barny", 10, employees);
+
+            Employee employee = new Employee("Stephen", 40, "Bulgaria");
+
+            bakery.Add(employee);
+            Console.WriteLine(bakery.Remove("Name"));
+
+            Employee secondEmployee = new Employee("Mark", 34, "UK");
+
+            bakery.Add(secondEmployee);
+
+            Employee oldestEmployee = bakery.GetOldestEmpoloyee();
+            Employee employeeName = bakery.GetEmployee("Stephen");
+
+            Console.WriteLine(oldestEmployee);
+            Console.WriteLine(employeeName);
+
+
+            Console.WriteLine(bakery.Count());
+
+            bakery.Report();
+        }
+    }
+}
