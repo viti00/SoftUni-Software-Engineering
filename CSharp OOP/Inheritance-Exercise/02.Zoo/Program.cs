5bc4a6925929d67af24ae9c159c2729de25e78d6
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-30 19:31:11 +0300

    Add files via upload

---
+++ CSharp OOP/Inheritance-Exercise/02.Zoo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string nameGorila = "GorilaJohn";
+            string nameBear = "BearGoGi";
+            string nameSnake = "SnakePepa";
+            string nameLizard = "LizardMito";
+
+            Bear bear = new Bear(nameBear);
+            Gorilla gorila = new Gorilla(nameGorila);
+            Snake snake = new Snake(nameSnake);
+            Lizard lizard = new Lizard(nameLizard);
+
+            Console.WriteLine(bear.Name);
+            Console.WriteLine(gorila.Name);
+            Console.WriteLine(snake.Name);
+            Console.WriteLine(lizard.Name);
+        }
+    }
+}
