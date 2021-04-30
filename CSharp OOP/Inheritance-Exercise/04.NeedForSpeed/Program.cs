4d0612edf21eabb4b3e5c7a480be1ca07df0460e
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-30 19:32:42 +0300

    Add files via upload

---
+++ CSharp OOP/Inheritance-Exercise/04.NeedForSpeed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int horsePower = 450;
+            double fuel = 100;
+
+            SportCar sportCar = new SportCar(horsePower, fuel);
+
+            double kilometers = 5;
+
+            sportCar.Drive(kilometers);
+
+
+            Console.WriteLine($"{sportCar.HorsePower} {sportCar.Fuel} {sportCar.DefaultFuelConsumption} {sportCar.FuelConsumption}");
+        }
+    }
+}
---
+++ CSharp OOP/Inheritance-Exercise/04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public int HorsePower { get; set; }
+
+        public double Fuel { get; set; }
+
+        public double DefaultFuelConsumption = 1.25;
+
+        public virtual double FuelConsumption { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.FuelConsumption = FuelConsumption + (kilometers * DefaultFuelConsumption);
+            this.Fuel = this.Fuel - (kilometers * DefaultFuelConsumption);
+        }
+    }
+}
