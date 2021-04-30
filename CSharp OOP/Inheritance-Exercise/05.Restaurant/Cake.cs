71598455b14e18551bd3617ead4064f59fe671d5
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-30 19:33:20 +0300

    Add files via upload

---
+++ CSharp OOP/Inheritance-Exercise/05.Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double cakeGrams = 250;
+        private const double cakeCalories = 1000;
+        private const decimal cakePrice = 5M;
+        public Cake(string name)
+            :base(name, cakePrice, cakeGrams, cakeCalories)
+        {
+           
+        }
+    }
+}
---
+++ CSharp OOP/Inheritance-Exercise/05.Restaurant/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = "IceCream";
+
+            Cake cake = new Cake(name);
+
+            Console.WriteLine($"{cake.Name} {cake.Price} {cake.Grams} {cake.Calories}");
+
+
+            string coffeeName = "NovaBraziliq";
+            double caffeine = 2.7;
+
+
+            Coffee coffee = new Coffee(coffeeName, caffeine);
+
+            Console.WriteLine($"{coffee.Name} {coffee.Price} {coffee.Mililiters} {coffee.Caffeine}");
+        }
+    }
+}
