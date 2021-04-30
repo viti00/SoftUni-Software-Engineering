1eb9b3d76b74868653b60f0c29d3da8f279b5202
viti00 <75797230+viti00@users.noreply.github.com>
2021-04-30 19:32:11 +0300

    Add files via upload

---
+++ CSharp OOP/Inheritance-Exercise/03.PlayersAndMonsters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string username = "Pesho";
+            int level = 33;
+
+            BladeKnight bladeKnight = new BladeKnight(username, level);
+
+            Console.WriteLine(bladeKnight);
+        }
+    }
+}
