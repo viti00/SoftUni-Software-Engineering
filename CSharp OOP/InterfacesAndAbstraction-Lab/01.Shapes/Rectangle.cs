b1bad01af90dc6f2ef0bb455baf9a28793957cfe
viti00 <75797230+viti00@users.noreply.github.com>
2021-05-18 14:10:53 +0300

    Add files via upload

---
+++ CSharp OOP/InterfacesAndAbstraction-Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+        public int Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
