5034cd38097874b9abb478bdbf592f54ecc9314a
viti00 <75797230+viti00@users.noreply.github.com>
2021-05-20 16:15:47 +0300

    Add files via upload

---
+++ CSharp OOP/InterfacesAndAbstraction-Exercise/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get => this.name; set => this.name = value; }
+        public int Age { get => this.age; set => this.age = value; }
+    }
+}
