b13930f1f7d59c7699c9a74c25ef3a66cf31ae68
viti00 <75797230+viti00@users.noreply.github.com>
2021-05-22 19:45:38 +0300

    Add files via upload

---
+++ CSharp OOP/Polymorphism-Lab/03.Shapes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rect = new Rectangle(2, 5);
+            Shape circle = new Circle(4);
+
+            Console.WriteLine(rect.CalculatePerimeter());
+            Console.WriteLine(rect.CalculateArea());
+            Console.WriteLine(rect.Draw());
+
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
