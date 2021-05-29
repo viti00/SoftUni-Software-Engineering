f1a6e0b9bea03a140ea5f71ac4aef9721724f547
viti00 <75797230+viti00@users.noreply.github.com>
2021-05-29 12:49:02 +0300

    Add files via upload

---
+++ CSharp OOP/ReflectionAndAttributes-Lab/05.CreateAttribute/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace AuthorProblem
+{
+    [Author("Venci")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ CSharp OOP/ReflectionAndAttributes-Lab/05.CreateAttribute/Tracker.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+
+            var methods = type.GetMethods((BindingFlags)28);
+
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute atr in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {atr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
