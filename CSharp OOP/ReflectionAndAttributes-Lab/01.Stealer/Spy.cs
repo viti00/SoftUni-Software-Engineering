7901b2ebcdec4efc23155b25262a2e69714a44e6
viti00 <75797230+viti00@users.noreply.github.com>
2021-05-29 12:48:03 +0300

    Add files via upload

---
+++ CSharp OOP/ReflectionAndAttributes-Lab/01.Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string nameOfClass, params string[] fields)
+        {
+            Type type = Type.GetType(nameOfClass);
+            FieldInfo[] classFields = type.GetFields((BindingFlags)60);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Class under investigation: {nameOfClass}");
+
+            object classInstance = Activator.CreateInstance(type, new object[] { });
+
+            foreach (var field in classFields)
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
