a35f0f54543f4c79a2286f6802d364d2cb85f438
viti00 <75797230+viti00@users.noreply.github.com>
2021-05-29 12:49:30 +0300

    Add files via upload

---
+++ CSharp OOP/ReflectionAndAttributes-Lab/AuthorProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Gogi")]
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+        [Author("penyo")]
+        public void SayHi()
+        {
+
+        }
+        [Author("tanq")]
+        public void SayBye()
+        {
+
+        }
+    }
+}
