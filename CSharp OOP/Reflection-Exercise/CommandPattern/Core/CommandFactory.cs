b69dd513bc57528109df13c3533cc3da0fa845c0
viti00 <75797230+viti00@users.noreply.github.com>
2021-06-09 22:24:29 +0300

    Add files via upload

---
+++ CSharp OOP/Reflection-Exercise/CommandPattern/Core/CommandFactory.cs
@@ -0,0 +1,28 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandFactory : ICommandFactory
+    {
+        private const string commandSufix = "Command";
+        public ICommand CreateCommand(string commandType)
+        {
+            var type = Assembly.GetEntryAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == $"{commandType}{commandSufix}");
+            if (type == null)
+            {
+                throw new ArgumentException($"{commandType} is invalid command type.");
+            }
+
+            ICommand instace = (ICommand)Activator.CreateInstance(type);
+
+            return instace;
+        }
+    }
+}
---
+++ CSharp OOP/Reflection-Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private readonly ICommandFactory commandFactory;
+
+        public CommandInterpreter()
+        {
+            this.commandFactory = new CommandFactory();
+        }
+
+        public string Read(string args)
+        {
+            string[] parts = args.Split();
+
+            string commandType = parts[0];
+
+            string[] commandArgs = parts.Skip(1).ToArray();
+
+            ICommand command = this.commandFactory.CreateCommand(commandType);
+
+            return command.Execute(commandArgs);
+        }
+    }
+}
