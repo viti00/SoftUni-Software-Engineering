895f59735ad28a1cc1af765af0ab4da066ad773e
viti00 <75797230+viti00@users.noreply.github.com>
2021-06-28 18:06:37 +0300

    Add files via upload

---
+++ CSharp OOP/UnitTest-Lab/Lab/Tests/AxeTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class AxeTests
+{
+    private int attack = 5;
+    private int durability = 6;
+    private Axe axe;
+    private Dummy dummy;
+    [SetUp]
+    public void SetUp()
+    {
+        axe = new Axe(attack, durability);
+        dummy = new Dummy(150, 150);
+    }
+
+    [Test]
+    public void When_AxeAttackAndDurabilityWasInicialized_ShoudBeSetCorrectly()
+    {
+
+        Assert.AreEqual(axe.AttackPoints, attack);
+
+        Assert.AreEqual(axe.DurabilityPoints, durability);
+    }
+
+    [Test]
+    public void When_InvokeAttackMethod_ShoudDecreaseAxeDurabilityWithOne()
+    {
+        axe.Attack(dummy);
+
+        Assert.AreEqual(durability - 1, axe.DurabilityPoints);
+    }
+
+    [Test]
+    public void When_AxeDurabilityIsNegativeOrZero_ShoudThrow()
+    {
+        Assert.That(() =>
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                axe.Attack(dummy);
+            }
+        }, Throws.InvalidOperationException.With.Message.EqualTo("Axe is broken."));
+    }
+}
---
+++ CSharp OOP/UnitTest-Lab/Lab/Tests/DummyTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class DummyTests
+{
+    private int health = 5000;
+    private int exp = 5000;
+    private Dummy dummy;
+    private Dummy deadDummy;
+
+    [SetUp]
+    public void SetUp()
+    {
+        dummy = new Dummy(health, exp);
+        deadDummy = new Dummy(-3, exp);
+    }
+    [Test]
+    public void When_DummyHealthInicialised_ShoudBeSetCorrectly()
+    {
+        Assert.AreEqual(dummy.Health, health);
+    }
+    [Test]
+    public void When_DummyTakeDamage_ShoudBeDecreaseHisHealth()
+    {
+        dummy.TakeAttack(12);
+
+        Assert.AreEqual(health - 12, dummy.Health);
+    }
+    [Test]
+    public void When_AttackedDeadDummy_ShoudTrhow()
+    {
+        Assert.That(() =>
+        {
+            deadDummy.TakeAttack(3);
+        }, Throws.InvalidOperationException.With.Message.EqualTo("Dummy is dead."));
+    }
+    [Test]
+    public void When_DummyIsDead_ShoudGiveExperience()
+    {
+        Assert.AreEqual(deadDummy.GiveExperience(), exp);
+    }
+    [Test]
+    public void When_DummyIsAllive_ShoudNotGiveExperience()
+    {
+        Assert.That(() =>
+        {
+            dummy.GiveExperience();
+        }, Throws.InvalidOperationException.With.Message.EqualTo("Target is not dead."));
+    }
+    [Test]
+    public void When_HealthIsPositive_ShouldBeAlive()
+    {
+        Assert.That(dummy.IsDead(), Is.EqualTo(false));
+    }
+
+    [Test]
+    public void When_HealthIsZero_ShouldBeDead()
+    {
+        dummy = new Dummy(0, exp);
+        Assert.That(dummy.IsDead(), Is.EqualTo(true));
+    }
+
+    [Test]
+    public void When_HealthIsNegative_ShouldBeDead()
+    {
+        Assert.That(deadDummy.IsDead(), Is.EqualTo(true));
+    }
+}
+
