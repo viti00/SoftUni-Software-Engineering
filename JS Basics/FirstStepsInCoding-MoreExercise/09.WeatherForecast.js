b283b886e9ec53c4b78283377c02984a7d676ba6
viti00 <75797230+viti00@users.noreply.github.com>
2021-07-23 18:57:54 +0300

    Add files via upload

---
+++ JS Basics/FirstStepsInCoding-MoreExercise/09.WeatherForecast.js
@@ -0,0 +1,12 @@
+function weatherForecast(input){
+    let weather = input;
+    if(weather == "sunny"){
+        console.log("It's warm outside!");
+    }
+    else{
+        console.log("It's cold outside!");
+    }
+}
+
+
+weatherForecast("cloudy");
