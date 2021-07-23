dfebae42ef795a0e8c8ce0956bacee6982c35caf
viti00 <75797230+viti00@users.noreply.github.com>
2021-07-23 18:55:45 +0300

    Add files via upload

---
+++ JS Basics/FirstStepsInCoding-MoreExercise/01.TrapeziodArea.js
@@ -0,0 +1,11 @@
+function calucateArea(input){
+    let sideB1 = Number(input[0]);
+    let sideB2 = Number(input[1]);
+    let height =  Number(input[2]);
+
+    let area = (sideB1 + sideB2) * height / 2;
+
+    console.log(area.toFixed(2));
+}
+
+calucateArea(["8", "13", "7"]);
