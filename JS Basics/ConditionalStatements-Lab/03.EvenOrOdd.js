9e7e709d193194a004a05fc13a1e82ef777dacaa
viti00 <75797230+viti00@users.noreply.github.com>
2021-07-26 17:24:18 +0300

    Add files via upload

---
+++ JS Basics/ConditionalStatements-Lab/03.EvenOrOdd.js
@@ -0,0 +1,12 @@
+function evenOrOdd(input){
+    let number = Number(input);
+
+    if (number % 2 == 0){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
+}
+
+evenOrOdd(["3"]);
