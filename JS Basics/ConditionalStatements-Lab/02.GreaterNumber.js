89cdc7447d4acf37b91aaa8e32a8028c67d93f44
viti00 <75797230+viti00@users.noreply.github.com>
2021-07-26 17:23:40 +0300

    Add files via upload

---
+++ JS Basics/ConditionalStatements-Lab/02.GreaterNumber.js
@@ -0,0 +1,13 @@
+function greatNumber(input){
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+
+    if(a > b){
+        console.log(a);
+    }
+    else{
+        console.log(b);
+    }
+}
+
+greatNumber(["3", "4"]);
