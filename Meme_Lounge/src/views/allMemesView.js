44a427f89f64c1b4de34a69eb07551590ebbb5fc
viti00 <75797230+viti00@users.noreply.github.com>
2022-10-07 20:05:05 +0300

    Delete Meme_Lounge directory

--- Meme_Lounge/src/views/allMemesView.js
+++
@@ -1,9 +0,0 @@
-import { getAllMemes } from "../api.js";
-import{allMemesTemplate} from "../templates/allMemesTemplate.js"
-
-
-export async function allMemeView(ctx){
-    const data = await getAllMemes();
-
-    ctx.render(allMemesTemplate(data))
-}
--- Meme_Lounge/src/views/homeView.js
+++
@@ -1,6 +0,0 @@
-import { homeTemplate } from "../templates/homeTemplate.js";
-
-
-export function homeView(ctx){
-    ctx.render(homeTemplate());
-}
